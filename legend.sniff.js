039a555693b6e7ea089b314e073041d3a9c77fa4
jimboy3000 <jimboy3100@yahoo.com>
2017-03-31 02:11:24 +0300

    Create legend.sniff.js

---
+++ legend.sniff.js
@@ -0,0 +1,11 @@
+//Legend Mod Sniff Mode by jimboy3100
+
+var url = window.location.href;
+localStorage.setItem("url", url);
+
+//document.title = "Legend mod v" + modVersion;
+//$("#leaderboard-hud > h4").text("LEGENDMOD.ML");
+
+//setTimeout(function () {
+//document.getElementById('create-party-btn').click();
+//}, 1000);
