f28c9ab565f4e159cd68879bbd202d42e6b219e0
jimboy3000 <jimboy3100@yahoo.com>
2017-03-31 06:27:25 +0300

    Update legendmod3.user.js

--- legendmod3.user.js
+++ legendmod3.user.js
@@ -1,27 +1,9 @@
-// ==UserScript==
-// @name         Legend Mod TEST1
-// @namespace    Legend Agario Mod
-// @version      2.0
-// @description  Legend Agario Mod - Communicate, Play All!
-// @homepage     http://www.legendmod.ml
-// @author       Jimboy3100
-// @icon         https://raw.githubusercontent.com/jimboy3100/legend.github.io/master/banners/smallbannerlegendclan.png
-// @match        http://agar.io/*
-// @downloadURL  https://jimboy3000.github.io/legendmod.user.js
-// @updateURL    https://jimboy3000.github.io/legendmod.user.js
-// @run-at       document-start
-// @grant        GM_xmlhttpRequest
-// @connect      agar.io
-// ==/UserScript==
-
-// Legend Mod by Jimboy3100
-// Start of script
 if (location.host == "agar.io" && location.pathname == "/") {
 
     location.href = "http://agar.io/legendmod" + window.location.search + location.hash;
     //return;
 }
-var modVersion = GM_info.script.version;
+
 var legendmod = '<script src="https://jimboy3000.github.io/legendmodstarter.js"></script>';
 
 
@@ -34,7 +16,7 @@ function inject(page) {
     page = page.replace("</body>", legendJSniffJS + legendarioJS + legendJS + "<script>init('" + modVersion + "');</script>" + "</body>");
     return page;
 }
-
+var modVersion = GM_info.script.version;
 window.stop();
 document.documentElement.innerHTML = "";
 GM_xmlhttpRequest({
