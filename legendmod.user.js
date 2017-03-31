4e6a7f3bb18b3cabe6899ef6806bac627060844b
jimboy3000 <jimboy3100@yahoo.com>
2017-03-31 04:18:13 +0300

    Update legendmod.user.js

--- legendmod.user.js
+++ legendmod.user.js
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 // Legend Mod by Jimboy3100
-
+// Start of script
 if (location.host == "agar.io" && location.pathname == "/") {
 
     location.href = "http://agar.io/legendmod" + window.location.search + location.hash;
@@ -46,13 +46,12 @@ var perfectCSS = '<link href="http://cdn.ogario.ovh/static/css/perfect-scrollbar
 
 var ytJS = '<script src="https://www.youtube.com/iframe_api"></script>';
 var faCSS = '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"></link>';
-//var gaJS = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');</script>";
 
 var modVersion = GM_info.script.version;
 
 function inject(page) {
     var page = page.replace("</head>", cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + faCSS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendJSniff2JS + legendarioSniffJS + ytJS + keyJS + "</head>");
-    //var _page = page.replace("</head>", cpickerCSS + toastrCSS + ogarioCSS + faCSS + cpickerJS + toastrJS + ogarioSniffJS + ytJS + gaJS + "</head>");
+
     
     page = page.replace(/<script.*?>[\s]*?.*?window\.NREUM[\s\S]*?<\/script>/, "");
     page = page.replace(/<script.*?src=".*?agario\.core\.js.*?><\/script>/, "");
