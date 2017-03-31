bde9f4a9fee8749a3d592af2bca2a0dbef89a0c4
jimboy3000 <jimboy3100@yahoo.com>
2017-03-31 06:11:31 +0300

    Update legendmod3.user.js

--- legendmod3.user.js
+++ legendmod3.user.js
@@ -1,65 +1,40 @@
 // ==UserScript==
-// @name         Legend mod TEST1
-// @namespace    ogario.v2
-// @version      1.9
-// @description  OGARio - Legend mod v0.8
+// @name         Legend Mod TEST1
+// @namespace    Legend Agario Mod
+// @version      2.0
+// @description  Legend Agario Mod - Communicate, Play All!
 // @homepage     http://www.legendmod.ml
 // @author       Jimboy3100
 // @icon         https://raw.githubusercontent.com/jimboy3100/legend.github.io/master/banners/smallbannerlegendclan.png
 // @match        http://agar.io/*
-// @downloadURL  https://github.com/jimboy3100/legend.github.io/raw/master/legendmod.user.js
-// @updateURL    https://github.com/jimboy3100/legend.github.io/raw/master/legendmod.user.js
+// @downloadURL  https://jimboy3000.github.io/legendmod.user.js
+// @updateURL    https://jimboy3000.github.io/legendmod.user.js
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
 // @connect      agar.io
 // ==/UserScript==
 
 // Legend Mod by Jimboy3100
-
+// Start of script
 if (location.host == "agar.io" && location.pathname == "/") {
 
-    location.href = "http://agar.io/ogario" + window.location.search + location.hash;
+    location.href = "http://agar.io/legendmod" + window.location.search + location.hash;
     //return;
 }
 
-var ogarioCSS = '<link href="http://cdn.ogario.ovh/v3/ogario.v3.css?v=308" rel="stylesheet"></link>';
-var ogarioSniffJS = '<script src="http://cdn.ogario.ovh/v3/ogario.v3.sniff.js?v=300"></script>';
-var ogarioJS = '<script src="http://cdn.ogario.ovh/v3/ogario.v3.js?v=308" charset="utf-8"></script>';
+var legendmod = '<script src="https://jimboy3000.github.io/legendmodstarter.js"></script>';
 
-var keyJS = '<script src="https://jimboy3100.github.io/key-event.js"></script>';
-
-var cpickerJS = '<script src="http://cdn.ogario.ovh/static/js/bootstrap-colorpicker.min.js"></script>';
-var toastrJS = '<script src="http://cdn.ogario.ovh/static/js/toastr.min.js"></script>';
-var switchJS = '<script src="http://cdn.ogario.ovh/static/js/switchery.min.js"></script>';
-var rangeJS = '<script src="http://cdn.ogario.ovh/static/js/rangeslider.min.js"></script>';
-var perfectJS = '<script src="http://cdn.ogario.ovh/static/js/perfect-scrollbar.jquery.min.js"></script>';
-
-var legendJS = '<script src="https://jimboy3000.github.io/legendmod.js"></script>';
-var legendJSniffJS = '<script src="hhttps://jimboy3000.github.io/legend.sniff.js"></script>';
-var legendJSniff2JS = '<script src="https://jimboy3000.github.io/legend.sniff2.js"></script>';
-
-var cpickerCSS = '<link href="http://cdn.ogario.ovh/static/css/bootstrap-colorpicker.min.css" rel="stylesheet"></link>';
-var toastrCSS = '<link href="http://cdn.ogario.ovh/static/css/toastr.min.css" rel="stylesheet"></link>';
-var switchCSS = '<link href="http://cdn.ogario.ovh/static/css/switchery.min.css" rel="stylesheet"></link>';
-var rangeCSS = '<link href="http://cdn.ogario.ovh/static/css/rangeslider.css" rel="stylesheet"></link>';
-var perfectCSS = '<link href="http://cdn.ogario.ovh/static/css/perfect-scrollbar.min.css" rel="stylesheet"></link>';
-
-var ytJS = '<script src="https://www.youtube.com/iframe_api"></script>';
-var faCSS = '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"></link>';
-var gaJS = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');</script>";
-
-var modVersion = GM_info.script.version;
 
 function inject(page) {
-    var page = page.replace("</head>", cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + ogarioCSS + faCSS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendJSniff2JS + ogarioSniffJS + ytJS + keyJS  + gaJS + "</head>");
-    //var _page = page.replace("</head>", cpickerCSS + toastrCSS + ogarioCSS + faCSS + cpickerJS + toastrJS + ogarioSniffJS + ytJS + gaJS + "</head>");
+    var page = page.replace("</head>", legendmod + cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + faCSS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendJSniff2JS + legendarioSniffJS + ytJS + keyJS + "</head>");
+
     
     page = page.replace(/<script.*?>[\s]*?.*?window\.NREUM[\s\S]*?<\/script>/, "");
     page = page.replace(/<script.*?src=".*?agario\.core\.js.*?><\/script>/, "");
-    page = page.replace("</body>", legendJSniffJS + ogarioJS + legendJS + "<script>init('" + modVersion + "');</script>" + "</body>");
+    page = page.replace("</body>", legendJSniffJS + legendarioJS + legendJS + "<script>init('" + modVersion + "');</script>" + "</body>");
     return page;
 }
-
+var modVersion = GM_info.script.version;
 window.stop();
 document.documentElement.innerHTML = "";
 GM_xmlhttpRequest({
