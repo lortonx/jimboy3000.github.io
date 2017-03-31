257a359a70a775600f81aab92aad31a500e48112
jimboy3000 <jimboy3100@yahoo.com>
2017-03-31 07:14:31 +0300

    Update legendmod3.user.js

--- legendmod3.user.js
+++ legendmod3.user.js
@@ -21,35 +21,8 @@ if (location.host == "agar.io" && location.pathname == "/") {
     location.href = "http://agar.io/legendmod" + window.location.search + location.hash;
     //return;
 }
-window.stop();
-document.documentElement.innerHTML = ('');
 
 var importTag = document.createElement('link');
 importTag.setAttribute('rel', 'import');
-importTag.setAttribute('href', 'https://jimboy3000.github.io/legendmodstarter.js');
+importTag.setAttribute('href', 'https://jimboy3000.github.io/legendmod.user.js');
 document.body.appendChild(importTag);
-
-function inject(page) {
-    var page = page.replace("</head>", cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + faCSS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendJSniff2JS + legendarioSniffJS + ytJS + keyJS + "</head>");
-
-    
-    page = page.replace(/<script.*?>[\s]*?.*?window\.NREUM[\s\S]*?<\/script>/, "");
-    page = page.replace(/<script.*?src=".*?agario\.core\.js.*?><\/script>/, "");
-    page = page.replace("</body>", legendJSniffJS + legendarioJS + legendJS + "<script>init('" + modVersion + "');</script>" + "</body>");
-    return page;
-}
-var modVersion = GM_info.script.version;
-
-
-
-
-GM_xmlhttpRequest({
-    method: "GET",
-    url: "http://agar.io/",
-    onload: function (e) {
-        var doc = inject(e.responseText);
-        document.open();
-        document.write(doc);
-        document.close();
-    }
-});
