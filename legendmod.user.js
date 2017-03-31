c9ec66c1cddd8dcfcb057bc70746bd07177a0ff5
jimboy3000 <jimboy3100@yahoo.com>
2017-03-31 06:02:49 +0300

    Update legendmod.user.js

--- legendmod.user.js
+++ legendmod.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Legend Mod TEST1
-// @namespace    Legend MOd
-// @version      2
-// @description  Testing Mod
+// @namespace    Legend Agario Mod
+// @version      2.0
+// @description  Legend Agario Mod - Communicate, Play All!
 // @homepage     http://www.legendmod.ml
 // @author       Jimboy3100
 // @icon         https://raw.githubusercontent.com/jimboy3100/legend.github.io/master/banners/smallbannerlegendclan.png
 // @match        http://agar.io/*
 // @downloadURL  https://jimboy3000.github.io/legendmod.user.js
-// @updateURL    https://jijmboy3000.github.io/legendmod.user.js
+// @updateURL    https://jimboy3000.github.io/legendmod.user.js
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
 // @connect      agar.io
