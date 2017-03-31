9aafaf2bd55af1cc26f8ab49ca3c38de48c1e531
jimboy3000 <jimboy3100@yahoo.com>
2017-03-31 04:01:35 +0300

    Update legendmod.user.js

--- legendmod.user.js
+++ legendmod.user.js
@@ -2,7 +2,7 @@
 // @name         Legend Mod TEST1
 // @namespace    Legend MOd
 // @version      2
-// @description  OGARio - Legend mod v0.8
+// @description  Testing Mod
 // @homepage     http://www.legendmod.ml
 // @author       Jimboy3100
 // @icon         https://raw.githubusercontent.com/jimboy3100/legend.github.io/master/banners/smallbannerlegendclan.png
