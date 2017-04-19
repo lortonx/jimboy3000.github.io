f4d56f708618c8c993423ef5637090dd0526749e
jimboy3000 <jimboy3100@yahoo.com>
2017-04-20 00:48:42 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
-* LEGEND modv2.079 by Jimboy3100   email:jimboy3100@hotmail.com
+* LEGEND modv2.070 by Jimboy3100   email:jimboy3100@hotmail.com
 *************/
 
 $("#region").on('change', function() { 
@@ -79,7 +79,7 @@ var setmessagecom="YES";
 var clanpassword;
 var searching;
 var timerId;
-var semimodVersion="82"; // the version 1.1-> 1.11
+var semimodVersion="83"; // the version 1.1-> 1.11
 T = {};
 var MSGCOMMANDS="";
 var MSGCOMMANDS2;
