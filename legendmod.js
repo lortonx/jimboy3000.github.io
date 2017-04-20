c56b808578b485a6ab5167caaa879ee5aae7e3fe
jimboy3000 <jimboy3100@yahoo.com>
2017-04-20 04:57:21 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
-* LEGEND mod v2.074 by Jimboy3100   email:jimboy3100@hotmail.com
+* LEGEND mod v2.075 by Jimboy3100   email:jimboy3100@hotmail.com
 *************/
 
 $("#region").on('change', function() { 
@@ -10,6 +10,22 @@ $("#region").on('change', function() {
       if (this.value == ":party") { $("#create-party-btn-2").click(); }
 		console.log( "Party stuff fixed" );})
 
+$('#gamemode').hide();
+$('#gamemode').after('<select id="gamemode2" class="form-control" required="" data-original-title="" title="" style="width: 32%;  display: inline-block; margin-left: 10px; margin-top: 6px">'+
+'<option value="" data-itr="page_gamemode_ffa">FFA</option>'+
+'<option value=":teams" data-itr="page_gamemode_teams">Teams</option>'+
+'<option value=":experimental" data-itr="page_gamemode_experimental">Experimental</option>'+
+'<option value=":party" data-itr="page_party">Party Mode</option>'+
+'</select>');
+$('#gamemode2').change(function(){MC.setGameMode($('#gamemode2').val());});		
+		
+setTimeout(function (){ $("#create-party-btn-2").click();},800);
+setTimeout(function (){ if($("#top5-hud").is(":visible")==false){$("#create-party-btn-2").click();}},1500);		  
+setTimeout(function (){ if($("#top5-hud").is(":visible")==false){$("#create-party-btn-2").click();}},2500);
+setTimeout(function (){ if($("#top5-hud").is(":visible")==false){$("#create-party-btn-2").click();}},3500);
+setTimeout(function (){ if($("#top5-hud").is(":visible")==false){$("#create-party-btn-2").click();}},4500);
+setTimeout(function (){ if($("#top5-hud").is(":visible")==false){$("#create-party-btn-2").click();}},5500);						
+		
 var currentIP = "0.0.0.0:0";
 var currentToken = "";
 var previousMode = localStorage.getItem("gamemode");
