fe1373816632898a8958714a3ec1e2880ff71219
jimboy3000 <jimboy3100@yahoo.com>
2017-06-02 09:22:32 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
-* LEGEND mod v2.029 by Jimboy3100   email:jimboy3100@hotmail.com
+* LEGEND mod v2.030 by Jimboy3100   email:jimboy3100@hotmail.com
 *************/
 
 $("#region").on('change', function() { 
@@ -585,9 +585,9 @@ setTimeout(function () {
 	$('#nick').blur(function(){if (clickedname=="YES"){
 	if($("#nick").val().length>=16){toastr["warning"]("[SERVER]: You cannot chat if player name > 15 chars:<br>"+ $('#nick').val())}}
 		var savedname=$("#nick").val();
-		if ($("#nick").val()=="EasterEgg1"){$("#nick").val(savedname);openbleedmod();} 
-		else if ($("#nick").val()=="EasterEgg2"){$("#nick").val(savedname);openrotatingmod();} 
-		else if ($("#nick").val()=="EasterEgg3"){$("#nick").val(savedname);openvidmod();} 
+		if ($("#nick").val()=="EasterEgg1"){toastr["info"]("Easter Egg 1 Activated").css("width", "210px");$("#nick").val(savedname);openbleedmod();} 
+		else if ($("#nick").val()=="EasterEgg2"){toastr["info"]("Easter Egg 2 Activated").css("width", "210px");$("#nick").val(savedname);openrotatingmod();} 
+		else if ($("#nick").val()=="EasterEgg3"){toastr["info"]('Easter Egg 3 Activated,<br>Video works better on vanilla, visit: <a target="_blank" href="https://github.com/jimboy3100">https://github.com/jimboy3100</a>' );$("#nick").val(savedname);openvidmod();} 
 	});
 	$('#clantag').mouseenter(function() {$('#clantag').css('background-color', '#000066');}).mouseleave(function() {$('#clantag').css('background-color', '');}); 		
 	$('#region').mouseenter(function() {$('#region').css('background-color', '#003300');MC.setQuality($('#quality').val());}).mouseleave(function() {$('#region').css('background-color', '');}); 		
