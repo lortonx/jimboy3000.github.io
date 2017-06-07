360a5d95ce4e93f53c68e846cac2d3e0d4a85a8f
jimboy3000 <jimboy3100@yahoo.com>
2017-06-08 00:26:07 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
-* LEGEND mod v2.070 by Jimboy3100   email:jimboy3100@hotmail.com
+* LEGEND mod v2.071 by Jimboy3100   email:jimboy3100@hotmail.com
 *************/
 
 $("#region").on('change', function() { 
@@ -194,7 +194,7 @@ if (languagemod==2){
 	setTimeout(function () {
 	$('#legendlanguages').val("2");
 	changeModLanguage();
-	}, 2500);
+	}, 7000);
 }
 
 
