fbac39ae28c7693ff8dbfae68430e6d8c5e7064a
jimboy3000 <jimboy3100@yahoo.com>
2017-06-09 19:14:59 +0300

    Update legend.sniff2.js

--- legend.sniff2.js
+++ legend.sniff2.js
@@ -2,3 +2,7 @@ setTimeout(function () {
 document.title = "Legend mod";
 $("#leaderboard-hud > h4").text("Leaderboard");
 }, 100);
+setTimeout(function () {
+$("#movingskins").css( { marginTop : "-15px" } );
+$("#movingskins").css( { marginBottom : "-30px" } );
+}, 12000);
