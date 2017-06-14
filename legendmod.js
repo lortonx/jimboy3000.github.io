46aeb04f98e592283af2e8f3fa232c49467994d7
jimboy3000 <jimboy3100@yahoo.com>
2017-06-14 07:26:14 +0300

    Create legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
-* LEGEND mod v2.093 by Jimboy3100   email:jimboy3100@hotmail.com
+* LEGEND mod v2.086 by Jimboy3100   email:jimboy3100@hotmail.com
 *************/
 
 $("#region").on('change', function() { 
