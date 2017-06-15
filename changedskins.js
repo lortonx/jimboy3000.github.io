a4fac44b023f3b0244131ac7a0e5de99ac7685f0
jimboy3000 <jimboy3100@yahoo.com>
2017-06-15 11:01:35 +0300

    Create changedskins.js

--- changedskins.js
+++ changedskins.js
@@ -1,4 +1,4 @@
-//v0.2
+//v0.3
 //1. Animated Skins
 
 (function agarXTRA(w) {
@@ -1655,7 +1655,7 @@ $("#administrationtoolshud").after('<button id="AdminBacktomenu" align="left" on
 		else { toastr["info"]('You must register your Clan Symbol first'); }
 	});
 		
-}, 8500);
+}, 7000);
 
 function disconnect2min(){
 	if (AdminRights==1){
