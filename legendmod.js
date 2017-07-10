ddf0e98133e08a2d4265d2a48063655468385920
jimboy3000 <jimboy3100@yahoo.com>
2017-07-11 01:54:41 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
-* LEGEND mod v2.119 by Jimboy3100   email:jimboy3100@hotmail.com
+* LEGEND mod v2.122 by Jimboy3100   email:jimboy3100@hotmail.com
 *************/
 
 $("#region").on('change', function() { 
