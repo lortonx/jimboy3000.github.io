16ee1242e13e1826d861a701a48d1fcb56c58835
jimboy3000 <jimboy3100@yahoo.com>
2017-08-01 12:10:49 +0300

    Update legendmod2.js

--- legendmod2.js
+++ legendmod2.js
@@ -1,5 +1,5 @@
 /*************
- * LEGEND mod v2.441 by Jimboy3100   email:jimboy3100@hotmail.com
+ * LEGEND mod v2.443 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 
 
@@ -121,7 +121,7 @@ var setyt = "YES";
 var clanpassword;
 var searching;
 var timerId;
-var semimodVersion = "35"; // the version 1.1-> 1.11
+var semimodVersion = "36"; // the version 1.1-> 1.11
 T = {};
 var MSGCOMMANDS = "";
 var MSGCOMMANDS2;
@@ -914,9 +914,9 @@ function init(modVersion) {
  //       $('#legendid').addClass('fa fa-thumbs-o-up').removeClass('quick-yt icon-youtube2');
         $("#more-skins").attr('href', 'https://jimboy3100.github.io/skins/skins.html');
         $(".quick-more-skins.ogicon-grin").attr('href', 'https://jimboy3100.github.io/skins/skins.html');
-        $("#quick-menu").append('<a id= "ModInfoQuick" class="fa fa-info" data-toggle="tooltip" data-container="body" data-placement="left" title="" data-original-title="Mod Info & Templates" onclick="openhelper();return false;"></a>');
-
-
+		$("#quick-menu").append('<a id= "LegGoogleForm" class="fa fa-check-square-o" data-toggle="tooltip" data-container="body" data-placement="left" title="" data-original-title="New Ideas & Statistics Form" href="https://docs.google.com/forms/d/1dpmqk_TMCmtT5fG26zqwqLVwbFh_XN2xgpXkZb5ZgXQ/" target="_blank"></a>'+
+		'<a id= "ModInfoQuick" class="fa fa-info" data-toggle="tooltip" data-container="body" data-placement="left" title="" data-original-title="Mod Info & Templates" onclick="openhelper();return false;"></a>');
+		
         // prevent edit
         $("#musicUrl").on("input", function() {
             $(this).attr("maxlength", "0");
@@ -2504,7 +2504,7 @@ function init(modVersion) {
             if (autoCoinBtn == "true") {
                 setTimeout(function() {
                     $("#autoCoinBtn").click();
-                }, 1000);
+                }, 5000);
             }
             //	if (copyGameNames == "true") {
             //		setTimeout(function () {$("#copyGameNames").click();}, 1000); }	
