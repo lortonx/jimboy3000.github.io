2b48bc1750b48af8a977170644448bbdf8d7bdf7
jimboy3000 <jimboy3100@yahoo.com>
2017-08-01 17:54:33 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
- * LEGEND mod v2.445 by Jimboy3100   email:jimboy3100@hotmail.com
+ * LEGEND mod v2.444 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 
 
@@ -2708,7 +2708,7 @@ function init(modVersion) {
 			$(".btn.btn-warning.btn-login-play.btn-needs-server").hide();
 			$(".btn.btn-play-guest.btn-success.btn-needs-server").css({'width': '100%'});
 			setTimeout(function() {
-			toastr["warning"]('<b>[SERVER]:</b> Legend Mod Private Server: ' + currentIP + '<br>Connect to any agar.io/?ip= server or make your own.<br>Library: <a target="_blank" href="https://jimboy3100.github.io/legendmod.user.js"><font color="yellow"><b><u>https://github.com/Megabyte918/MultiOgar-Edited</u></b></font><br>Play agario like games if you know the IP of servers', '', '{ timeOut: 10000, extendedTimeOut: 10000 }').css("width", "420px");
+			toastr["warning"]('<b>[SERVER]:</b> Legend Mod Private Server: ' + currentIP + '<br>Connect to any agar.io/?ip= server or make your own.<br>Library: <a target="_blank" href="https://jimboy3100.github.io/legendmod.user.js"><font color="yellow"><b><u>https://github.com/Megabyte918/MultiOgar-Edited</u></b></font><br>Play <<agario-like>> games if you know the IP of servers', '', '{ timeOut: 10000, extendedTimeOut: 10000 }').css("width", "420px");
             $("#server").hide();
             $("#connect2").hide();			
 			}, 3000); 
