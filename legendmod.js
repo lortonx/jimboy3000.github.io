1c2eff52076d3b28fe69148009e22c6332df5a72
jimboy3000 <jimboy3100@yahoo.com>
2017-08-01 16:37:14 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
- * LEGEND mod v2.452 by Jimboy3100   email:jimboy3100@hotmail.com
+ * LEGEND mod v2.453 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 
 
@@ -2699,6 +2699,8 @@ function init(modVersion) {
 			$(".btn.btn-play-guest.btn-success.btn-needs-server").css({'width': '100%'});
 			setTimeout(function() {
 			toastr["warning"]('<b>[SERVER]:</b> Legend Mod Private Server: ' + currentIP + '<br>Connect to any agar.io/?ip= server or make your own.<br>Library: <a target="_blank" href="https://jimboy3100.github.io/legendmod.user.js"><font color="yellow"><b><u>https://github.com/Megabyte918/MultiOgar-Edited</u></b></font>', '', '{ timeOut: 10000, extendedTimeOut: 10000 }').css("width", "420px");
+            $("#server").hide();
+            $("#connect2").hide();			
 			}, 3000); 
 			
 		}
