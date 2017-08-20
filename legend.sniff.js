fd4d0851d33b77c1e2bfb781067228dd8c0db907
jimboy3000 <jimboy3100@yahoo.com>
2017-08-20 07:28:05 +0300

    Update legend.sniff.js

--- legend.sniff.js
+++ legend.sniff.js
@@ -108,23 +108,3 @@ $( "body" ).append('<div id="imagebig"><iframe id="loaderIframeIcon1" src="https
       
    });
 })('tcm_old_store_skins', window, document);
-
-
-
-
-//load gooogle analytics
-/*
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  window.ga('create', 'UA-92655864-1', 'auto');
-  window.ga('send', 'pageview');
-*/
-//document.title = "Legend mod v" + modVersion;
-//$("#leaderboard-hud > h4").text("LEGENDMOD.ML");
-
-//setTimeout(function () {
-//document.getElementById('create-party-btn').click();
-//}, 1000);
