b6098fbef5e0e4cc6cab156e6cff83ec51bcd47f
jimboy3000 <jimboy3100@yahoo.com>
2017-08-20 07:57:43 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
- * LEGEND mod v2.471 by Jimboy3100   email:jimboy3100@hotmail.com
+ * LEGEND mod v2.472 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 loadericon();
 	
@@ -332,12 +332,11 @@ $("body").on('DOMNodeInserted', ".toast.toast-warning", function() {
 		$(".toast.toast-warning").remove();
 		setTimeout(function () {
 		spectate();
+		},200);
 		$("#overlays").show();
 		$(".center-container.ogario-menu").show();
 		$(".side-container.right-container").show();
 		$(".side-container.left-container").show();		
-		},200);
-
 	
 		MC.setQuality($('#quality').val());
 		if($('#region>option:nth-child(1)').val()!=":PrS")	{
@@ -363,12 +362,11 @@ $("body").on('DOMSubtreeModified', "#chat-box", function() {
 	        $(".command-text").text(Premadeletter0);
 		setTimeout(function () {
 		spectate();
+		},200);
 		$("#overlays").show();
 		$(".center-container.ogario-menu").show();
 		$(".side-container.right-container").show();
 		$(".side-container.left-container").show();		
-		},200);
-
 		
 		MC.setQuality($('#quality').val());
 		if($('#region>option:nth-child(1)').val()!=":PrS")	{
@@ -1882,9 +1880,14 @@ function init(modVersion) {
 
 
         $("#troll1Btn").click(function() {
+			
             var checked = !($(this).attr('aria-pressed') == "true");
             if (checked) {
                 localStorage.setItem("troll1Btn", true);
+				if (dyinglight1load=="yes") { 
+					toastr["info"]("When Dying Light Expansion is enabled, Troll on death cannot occur").css("width", "300px");
+					return false;
+				}
                 settroll1true();
                 whenplayerdies();
                 $(this).html('<i class="fa fa-bath"></i>' + Premadeletter56);
