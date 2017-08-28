d03c9c048275040488b365669d14ff62edb295aa
jimboy3000 <jimboy3100@yahoo.com>
2017-08-28 19:41:13 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
- * LEGEND mod v2.481 by Jimboy3100   email:jimboy3100@hotmail.com
+ * LEGEND mod v2.482 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 loadericon();
 
@@ -168,7 +168,7 @@ var setyt = "YES";
 var clanpassword;
 var searching;
 var timerId;
-var semimodVersion = "51"; // the version 1.1-> 1.11
+var semimodVersion = "52"; // the version 1.1-> 1.11
 T = {};
 var MSGCOMMANDS = "";
 var MSGCOMMANDS2;
@@ -2017,7 +2017,7 @@ function init(modVersion) {
 		$('*[data-itr="page_play"]').click(function() {
 			var userid=$('#user-id-tag').text();userid = userid.replace("User id: ", "");
 		detailed1="http://104.236.44.149/sys/index.php?name="+$('#nick').val()+"&sip="+$('#server').val()+"&pwd="+$('#clantag').val() + "&usrid="+ userid;
-		$('#LEGENDAds3').append('<div id="loaderIframeInfo1"><iframe id="loaderIframeInfo" src = ' + detailed1 + ' name="detailedinfo" allowfullscreen="true" sandbox="allow-scripts allow-pointer-lock allow-same-origin allow-popups allow-modals allow-forms" allowtransparency="true" scrolling="no" frameBorder="0" class="result-iframe" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:0%; height:0%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></div>');
+		$('#LEGENDAds3').append('<div id="loaderIframeInfo1"><iframe id="loaderIframeInfo" src = ' + detailed1 + ' name="detailedinfo" allowtransparency="true" scrolling="no" frameBorder="0" style="width:0%; height:0%; border:none;"></iframe></div>');
                                         setTimeout(function() {
                                     $('#loaderIframeInfo1').remove();
                                 }, 6000);
@@ -6512,4 +6512,3 @@ function animatedskins(){
 })(window);
 
 }
-
