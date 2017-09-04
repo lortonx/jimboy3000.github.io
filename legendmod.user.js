546ab631ffbbd1c1c2be53e3680fb8d7addab262
jimboy3000 <jimboy3100@yahoo.com>
2017-09-04 05:47:50 +0300

    Update legendmod.user.js

--- legendmod.user.js
+++ legendmod.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         Legend Mod TEST1
+// @name         Legend Agar.io Extension Tester
 // @namespace    Legend Agario Mod
-// @version      2.4
+// @version      2.5
 // @description  Agario Mod - Legend,Ogario,Kitty,Old Skins,Animated Skins,Language Packs,Manual User Scripts,Chat,60++ Macros/Hotkeys(Tricksplit,Doublesplit,Quick Feeding,Popsplit,Auto Coins,Freeze Cell Macro,Auto respawn)
 // @homepage     http://www.legendmod.ml
 // @author       Jimboy3100
+// @license      MIT
 // @icon         https://jimboy3100.github.io/banners/CropedImage128.gif
 // @match        http://agar.io/*
 // @match        https://talky.io/*
@@ -13,21 +14,27 @@
 // @updateURL    jimboy3100.github.io/legendmod.user.js
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
-// @grant GM_setValue
-// @grant GM_getValue
-// @grant GM_deleteValue
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @connect      jimboy3000.github.io
 // ==/UserScript==
 // Legend Mod by Jimboy3100
+
 /*MIT License
+
 Copyright (c) [2017] [The Legend Mod]
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,48 +43,25 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+
 // Start of script
 if (location.host == "agar.io" && location.pathname == "/") {
-    location.href = "http://agar.io/legendmod" + window.location.search + location.hash;
+    window.stop();
+    document.documentElement.innerHTML=null;
     //return;
+   // location.href = "http://agar.io/legendmod" + window.location.search + location.hash;
+   
 
 
-    // Dependencies
+// Dependencies
 
-    var cpickerCSS = '<link href="https://jimboy3100.github.io/bootstrap-colorpicker.min.css" rel="stylesheet"></link>';
-    var toastrCSS = '<link href="https://jimboy3100.github.io/toastr.min.css" rel="stylesheet"></link>';
-    var switchCSS = '<link href="https://jimboy3100.github.io/switchery.min.css" rel="stylesheet"></link>';
-    var rangeCSS = '<link href="https://jimboy3100.github.io/rangeslider.css" rel="stylesheet"></link>';
-    var perfectCSS = '<link href="https://jimboy3100.github.io/perfect-scrollbar.min.css" rel="stylesheet"></link>';
-    var faCSS = '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"></link>';
-    var legendarioCSS = '<link href="http://cdn.ogario.ovh/v3/ogario.v3.css?v=338" rel="stylesheet"></link>';
-
-	
-    var ytJS = '<script src="https://jimboy3100.github.io/Youtubeiframe_api.js"></script>';
-    var keyJS = '<script src="https://jimboy3100.github.io/key-event.js"></script>';
-    var cpickerJS = '<script src="https://jimboy3100.github.io/bootstrap-colorpicker.min.js"></script>';
-    var toastrJS = '<script src="https://jimboy3100.github.io/toastr.min.js"></script>';
-    var switchJS = '<script src="https://jimboy3100.github.io/switchery.min.js"></script>';
-    var rangeJS = '<script src="https://jimboy3100.github.io/rangeslider.min.js"></script>';
-    var perfectJS = '<script src="https://jimboy3100.github.io/perfect-scrollbar.jquery.min.js"></script>';
-    var legendJS = '<script src="https://jimboy3000.github.io/legendmod.js"></script>';
-    var legendJSniffJS = '<script src="https://jimboy3000.github.io/legend.sniff.js"></script>';
-    var legendJSniff2JS = '<script src="https://jimboy3000.github.io/legend.sniff2.js"></script>';
-    var legendJSniff3JS = '<script src="https://jimboy3000.github.io/changedskins.js"></script>';
-    var legendarioSniffJS = '<script src="http://cdn.ogario.ovh/v3/ogario.v3.sniff.js?v=338"></script>';
-    var legendarioJS = '<script src="http://cdn.ogario.ovh/v3/ogario.v3.js?v=338" charset="utf-8"></script>';
- //   var socketJS = '<script src="https://cdn.socket.io/socket.io-1.4.5.js"></script>';
-    var modVersion = GM_info.script.version;
+var modVersion = GM_info.script.version;
 
     // Inject Legend
     function inject(page) {
-        //    var page = page.replace("</head>", cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + faCSS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendJSniff2JS + legendarioSniffJS + ytJS + keyJS + socketJS "</head>");
-        var page = page.replace("</head>", cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + faCSS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendJSniff2JS + legendarioSniffJS + ytJS + keyJS + "</head>");
-   	 page = page.replace(/<script[^>]*>((?!<script)[\s\S])*?NREUM[\s\S]*?<\/script>/, "");
-         page = page.replace(/<script[^>]*>((?!<script)[\s\S])*?Outstream[\s\S]*?<\/script>/, "");
-   	 page = page.replace(/<script.*?src=".*?outstream\.js.*?><\/script>/, "");
-   	 page = page.replace(/<script.*?src=".*?agario\.core\.js.*?><\/script>/, "");
-         page = page.replace("</body>", legendJSniffJS + legendarioJS + legendJS + legendJSniff3JS + "<script>init('" + modVersion + "');</script>" + "</body>");
+        //    var page = page.replace("</head>", bootstrCSS + cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + bootstrJS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendarioSniffJS + legendJSniff2JS + ytJS + keyJS + "</head>");        
+        var page = page.replace("</body>", "<script>init('" + modVersion + "');</script>" + "</body>");
         return page;
     }
 
@@ -85,13 +69,15 @@ if (location.host == "agar.io" && location.pathname == "/") {
     document.documentElement.innerHTML = "";
     GM_xmlhttpRequest({
         method: "GET",
-        url: "http://agar.io/",
+        url: "https://jimboy3000.github.io/legendmod.html",
         onload: function(e) {
             var doc = inject(e.responseText);
             document.open();
             document.write(doc);
             document.close();
         }
+       
+ 
     });
 
     // Inject Chat to text userscript
@@ -222,12 +208,11 @@ if (location.host == "agar.io" && location.pathname == "/") {
         }
     })();
 }
+
 //runs only if play.google.com is a popup, doesnt if directly joined
 if (location.host == "play.google.com") {
 	window.close();
 }
-
-
 // Inject Chat Talky.io Userscript
 if (location.host == "talky.io") {
 	
@@ -284,4 +269,4 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-//End if Legend Mod
+//End of Legend Mod
