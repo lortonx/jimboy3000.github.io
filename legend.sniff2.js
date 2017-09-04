384403eafc9a85f2d3290468eb3ede88b90cdec1
jimboy3000 <jimboy3100@yahoo.com>
2017-09-04 11:27:13 +0300

    Update legend.sniff2.js

--- legend.sniff2.js
+++ legend.sniff2.js
@@ -1,8 +1,13 @@
-setTimeout(function () {
+//Legend Mod Sniff2 by jimboy3100
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://jimboy3100.github.io/banners/icon32croped.ico.gif';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
 document.title = "Legend mod";
-$("#leaderboard-hud > h4").text("Leaderboard");
-}, 100);
+
 setTimeout(function () {
-$("#movingskins").css( { marginTop : "-15px" } );
-$("#movingskins").css( { marginBottom : "-30px" } );
-}, 12000);
+$("#leaderboard-hud > h4").text("Leaderboard");
+}, 500);
