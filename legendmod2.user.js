3a1cb0afbd6766579ddc3c029279d50414e17a17
jimboy3000 <jimboy3100@yahoo.com>
2017-09-04 03:01:43 +0300

    Update legendmod2.user.js

--- legendmod2.user.js
+++ legendmod2.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         Legend Mod TEST2
+// @name         Legend Agar.io Extension
 // @namespace    Legend Agario Mod
-// @version      2.2
+// @version      2.4
 // @description  Agario Mod - Legend,Ogario,Kitty,Old Skins,Animated Skins,Language Packs,Manual User Scripts,Chat,60++ Macros/Hotkeys(Tricksplit,Doublesplit,Quick Feeding,Popsplit,Auto Coins,Freeze Cell Macro,Auto respawn)
 // @homepage     http://www.legendmod.ml
 // @author       Jimboy3100
+// @license      MIT
 // @icon         https://jimboy3100.github.io/banners/CropedImage128.gif
 // @match        http://agar.io/*
 // @match        https://talky.io/*
@@ -13,11 +14,13 @@
 // @updateURL    jimboy3100.github.io/legendmod.user.js
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
-// @grant GM_setValue
-// @grant GM_getValue
-// @grant GM_deleteValue
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @connect      jimboy3000.github.io
 // ==/UserScript==
 // Legend Mod by Jimboy3100
+
 /*MIT License
 
 Copyright (c) [2017] [The Legend Mod]
@@ -40,46 +43,25 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+
 // Start of script
 if (location.host == "agar.io" && location.pathname == "/") {
-    location.href = "http://agar.io/legendmod" + window.location.search + location.hash;
+    window.stop();
+    document.documentElement.innerHTML=null;
     //return;
+   // location.href = "http://agar.io/legendmod" + window.location.search + location.hash;
+   
 
 
-    // Dependencies
+// Dependencies
 
-    var cpickerCSS = '<link href="https://jimboy3100.github.io/bootstrap-colorpicker.min.css" rel="stylesheet"></link>';
-    var toastrCSS = '<link href="https://jimboy3100.github.io/toastr.min.css" rel="stylesheet"></link>';
-    var switchCSS = '<link href="https://jimboy3100.github.io/switchery.min.css" rel="stylesheet"></link>';
-    var rangeCSS = '<link href="https://jimboy3100.github.io/rangeslider.css" rel="stylesheet"></link>';
-    var perfectCSS = '<link href="https://jimboy3100.github.io/perfect-scrollbar.min.css" rel="stylesheet"></link>';
-    var faCSS = '<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"></link>';
-    var legendarioCSS = '<link href="https://jimboy3100.github.io/legend.css?v=333" rel="stylesheet"></link>';
-    //var legendarioCSS = '<link href="https://jimboy3100.github.io/legend.css?v=333" rel="stylesheet"></link>';
-
-    var ytJS = '<script src="https://jimboy3100.github.io/Youtubeiframe_api.js"></script>';
-    var keyJS = '<script src="https://jimboy3100.github.io/key-event.js"></script>';
-    var cpickerJS = '<script src="https://jimboy3100.github.io/bootstrap-colorpicker.min.js"></script>';
-    var toastrJS = '<script src="https://jimboy3100.github.io/toastr.min.js"></script>';
-    var switchJS = '<script src="https://jimboy3100.github.io/switchery.min.js"></script>';
-    var rangeJS = '<script src="https://jimboy3100.github.io/rangeslider.min.js"></script>';
-    var perfectJS = '<script src="https://jimboy3100.github.io/perfect-scrollbar.jquery.min.js"></script>';
-    var legendJS = '<script src="https://jimboy3000.github.io/legendmod2.js"></script>';
-    var legendJSniffJS = '<script src="https://jimboy3000.github.io/legend.sniff.js"></script>';
-    var legendJSniff2JS = '<script src="https://jimboy3000.github.io/legend.sniff2.js"></script>';
-    var legendJSniff3JS = '<script src="https://jimboy3000.github.io/changedskins.js"></script>';
-    var legendarioSniffJS = '<script src="http://cdn.ogario.ovh/v3/ogario.v3.sniff.js?v=333"></script>';
-    var legendarioJS = '<script src="http://cdn.ogario.ovh/v3/ogario.v3.js?v=333" charset="utf-8"></script>';
-    var socketJS = '<script src="https://cdn.socket.io/socket.io-1.4.5.js"></script>';
-    var modVersion = GM_info.script.version;
+var modVersion = GM_info.script.version;
 
     // Inject Legend
     function inject(page) {
-        //    var page = page.replace("</head>", cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS  + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendarioSniffJS + legendJSniff2JS + ytJS + keyJS + "</head>");
-        var page = page.replace("</head>", cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + faCSS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendJSniff2JS + legendarioSniffJS + ytJS + keyJS + socketJS + "</head>");
-        page = page.replace(/<script.*?>[\s]*?.*?window\.NREUM[\s\S]*?<\/script>/, "");
-        page = page.replace(/<script.*?src=".*?agario\.core\.js.*?><\/script>/, "");
-        page = page.replace("</body>", legendJSniffJS + legendarioJS + legendJS + legendJSniff3JS + "<script>init('" + modVersion + "');</script>" + "</body>");
+        //    var page = page.replace("</head>", bootstrCSS + cpickerCSS + toastrCSS + switchCSS + rangeCSS + perfectCSS + legendarioCSS + bootstrJS + cpickerJS + toastrJS + switchJS + rangeJS + perfectJS + legendarioSniffJS + legendJSniff2JS + ytJS + keyJS + "</head>");        
+        var page = page.replace("</body>", "<script>init('" + modVersion + "');</script>" + "</body>");
         return page;
     }
 
@@ -87,13 +69,15 @@ if (location.host == "agar.io" && location.pathname == "/") {
     document.documentElement.innerHTML = "";
     GM_xmlhttpRequest({
         method: "GET",
-        url: "http://agar.io/",
+        url: "https://jimboy3000.github.io/legendmod.html",
         onload: function(e) {
             var doc = inject(e.responseText);
             document.open();
             document.write(doc);
             document.close();
         }
+       
+ 
     });
 
     // Inject Chat to text userscript
@@ -224,12 +208,11 @@ if (location.host == "agar.io" && location.pathname == "/") {
         }
     })();
 }
+
 //runs only if play.google.com is a popup, doesnt if directly joined
 if (location.host == "play.google.com") {
 	window.close();
 }
-
-
 // Inject Chat Talky.io Userscript
 if (location.host == "talky.io") {
 	
@@ -286,4 +269,4 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-//End if Legend Mod
+//End of Legend Mod
