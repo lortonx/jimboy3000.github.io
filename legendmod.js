5b7e1b0145eb6891004b335896dcddc37c04699a
jimboy3000 <jimboy3100@yahoo.com>
2017-09-09 22:45:31 +0300

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
- * LEGEND mod v2.595 by Jimboy3100   email:jimboy3100@hotmail.com
+ * LEGEND mod v2.593 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 loadersetings();
 loadericon();
