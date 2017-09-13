3af44f82984ddad25f03aea9d040c0e328e89417
jimboy3000 <jimboy3100@yahoo.com>
2017-09-14 00:15:19 +0300

    Update legendmod2.js

--- legendmod2.js
+++ legendmod2.js
@@ -2336,11 +2336,11 @@ function init(modVersion) {
 				'Current <font color="yellow"><b>v2.5</b></font> is <b>BETA</b>. <font color="yellow"><b>v2.4</b></font> is stable. '+
 				'<br>Visit: <a target="_blank" href="https://jimboy3100.github.io/goodoldlegendmod.user.js"><font color="red"><b><u>www.legendmod.ml</u></b></font></a>'+
 				'<br><font style="color:#018cf6; font-size:16px; text-align:center"><b>What\'s new:</font></b><font color="black">'+
-				'<br><b>In this version, there is <b>NO</b> agar.io library at <b>ALL<br>'+
+				'<br><b>In this version, there are <b>NO</b> agar.io librares at <b>ALL.<br>'+
 				'There we no more conflicts because official updates'+
 				'<br>With legend mod, <a target="_blank" href="https://github.com/jimboy3100/jimboy3100.github.io"><font color="red"><b><u>libraries</u></b></font></a> users can play on Private or Official Agario servers.'+
 				'<br>We don\'t corrupt the game, but we make it even better.'+
-				'<br><i>Please Support Agar.io official game, by bying skins</i></font></div>', '', '{ timeOut: 15000, extendedTimeOut: 15000 }').css("width", "500px");			
+				'<br><i>Please Support Agar.io official game, by bying skins.</i></font></div>', '', '{ timeOut: 15000, extendedTimeOut: 15000 }').css("width", "500px");			
 			  }
 			else{
 			//  toastr["info"](Premadeletter17 + ' <b><font color="red">' + tag1 + '</font></b>!<br>When experiencing lag, press <font color="red"><b>'+$("#hk-showSkins").val()+'</b></font> to disable skins. <br><i> Less to draw means better performance.</i>').css("width", "380px");
