54fb8be28a200a66e413fc45894f8acf588cb996
jimboy3000 <jimboy3100@yahoo.com>
2017-12-04 17:44:56 +0200

    Update legendmod.js

--- legendmod.js
+++ legendmod.js
@@ -1,5 +1,5 @@
 /*************
- * LEGEND mod v2.547 by Jimboy3100   email:jimboy3100@hotmail.com
+ * LEGEND mod v2.548 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 var semimodVersion = "06"; // the version 1.1-> 1.11
  
@@ -32,11 +32,11 @@ var oldgamemode=$("#gamemode");
 		'<option value=":PrS0" data-itr="PrS0">-SELECT-</option>'+		
 		'<option value=":PrS1" data-itr="PrS1">1vs1 Arena(1)</option>'+
 		'<option value=":PrS2" data-itr="PrS2">1vs1 Arena(2)</option>'+
-		'<option value=":PrS3" data-itr="PrS3">Party Server(1)</option>'+
+		'<option value=":PrS3" data-itr="PrS3">Party Server(1)</option>');
 //		'<option value=":PrS4" data-itr="PrS4">Party Server(2)</option>'+
 //		'<option value=":PrS6" data-itr="PrS6">Instant Merge(1)</option>'+
 //		'<option value=":PrS5" data-itr="PrS5">Instant Merge(2)</option>'+
-		'<option value=":PrS7" data-itr="PrS7">Experimental</option>');
+//		'<option value=":PrS7" data-itr="PrS7">Experimental</option>');
 //		'<option value=":PrS8" data-itr="PrS8">Virus Mode</option>');
 //		'<option value=":PrS9" data-itr="PrS9">Small Bots</option>');	
 		
