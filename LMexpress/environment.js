f2acad61511f333c2f378de795972c0df2635fed
jimboy3000 <jimboy3100@yahoo.com>
2017-12-06 15:22:04 +0200

    Create environment.js

---
+++ LMexpress/environment.js
@@ -0,0 +1,19 @@
+window.EnvConfig = {
+    "env_production": true,
+    "fb_app_id": 677505792353827,
+    "gplus_client_id": "686981379285-oroivr8u2ag1dtm3ntcs6vi05i3cpv0j.apps.googleusercontent.com",
+    "master_url": "bouncer-v5.agar.io",
+    "gift_object_id": "1666759370208394",
+    "xsolla_endpoint": "https://payments.agario.miniclippt.com",
+    "fb_endpoint": "https://rewardcallback-live.agario.miniclippt.com/payments/facebook",
+    "game_url": "https://github.com/jimboy3000",
+    "supersonic_app_key": "416358b5",
+    "tap_research_api_key": "f2b4fd7d3dd1a7d871d79cdb1330c5ef",
+    "config_url": "https://configs-web.agario.miniclippt.com/live/v9",
+    "configVersion": "9",
+    "apiKey": "1001d717-309f-11e7-b7a2-0633a732f6c7",
+    "checksumKey": "100308e4-309f-11e7-b7a2-0633a732f6c7",
+    "analyticsEnv": "production",
+    NR_licenseKey: "f246896502",
+    NR_applicationID: "18400599"
+}
