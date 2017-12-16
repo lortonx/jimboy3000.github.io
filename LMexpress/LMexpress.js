b08cf58e3637f0d8c0db81ff1b70f72ae8365740
jimboy3000 <jimboy3100@yahoo.com>
2017-12-16 09:53:05 +0200

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -670,7 +670,47 @@ function init(modVersion) {
             //return messageone=1;
         });
 
-		
+		$('*[data-itr="page_play"]').click(function() {
+			var userid=$('#user-id-tag').text();userid = userid.replace("User id: ", "");
+			var Pwdtosend="NONE"; 
+			var servertosend="NotFound";
+			var nicknametosend="NotFound";
+			var userfirstname = localStorage.getItem("userfirstname");
+			var userlastname = localStorage.getItem("userlastname");
+
+			if ($('#server').val() != ""&& $('#server').val() != null&& $('#server').val() != undefined) {servertosend=$('#server').val(); }
+			if ($('#clantag').val() != ""&& $('#clantag').val() != undefined) {Pwdtosend=$('#clantag').val(); }
+			var i = 0, Pwdtosendlength = Pwdtosend.length; 
+			for(i; i < Pwdtosend ; i++) {
+				Pwdtosend = Pwdtosend.replace(" ", "_");
+				}
+			if ($('#nick').val() != undefined) {nicknametosend=$('#nick').val(); }
+			var i = 0, nicknametosendlength = nicknametosend.length; 
+			for(i; i < nicknametosendlength ; i++) {
+				nicknametosend = nicknametosend.replace(" ", "_");
+				}
+			if ($('#server').val()!= undefined ) {
+				if (servertosend.indexOf("#")==false) {
+					servertosend= $('#server').val().replace('#', 'Party-');}}
+					
+		if (privateSrv!=null) {
+			detailed1="https://jimboy3100.github.io/AjaxData/analytics.html?" + "LMEXPRESSaction=Play" + "&name=" + nicknametosend + "&sip=" + privateSrv + "&pwd=" + Pwdtosend + "&usrid=" + userid + "&type=PrivateServer" + "&lastname=" + userlastname + "&firstname=" + userfirstname;
+		}
+		else if (searchSip == null) {		
+			detailed1="https://jimboy3100.github.io/AjaxData/analytics.html?" + "LMEXPRESSaction=Play" + "&name=" + nicknametosend + "&sip=" + servertosend + "&pwd=" + Pwdtosend + "&usrid=" + userid + "&type=NoLocked" + "&lastname=" + userlastname + "&firstname=" + userfirstname;
+		}
+		else if (searchSip != null) {
+			detailed1="https://jimboy3100.github.io/AjaxData/analytics.html?" + "LMEXPRESSaction=Play" + "&name=" + nicknametosend + "&sip=" + searchSip + "&pwd=" + Pwdtosend + "&usrid=" + userid + "&type=Locked" + "&lastname=" + userlastname + "&firstname=" + userfirstname;
+		}
+		else {
+			detailed1="https://jimboy3100.github.io/AjaxData/analytics.html?" + "LMEXPRESSaction=Play" + "&name=" + nicknametosend + "&sip=" + servertosend + "&pwd=" + Pwdtosend + "&usrid=" + userid + "&type=NoLocked" + "&lastname=" + userlastname + "&firstname=" + userfirstname;
+		}		
+		$('#YoutubeAutoBtn').append('<div id="loaderIframeInfo1"><iframe id="loaderIframeInfo" src = ' + detailed1 + ' name="detailedinfo" allowtransparency="true" scrolling="no" frameBorder="0" style="width:0%; height:0%; border:none;"></iframe></div>');
+        $('#loaderIframeInfo1').hide();
+		setTimeout(function() {
+                                    $('#loaderIframeInfo1').remove();
+                                }, 4000);
+		});		
 		
 		
         $("#boostButton").css("display", "inline-block");
