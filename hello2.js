b16584a0d5aa35e21491ead983aabdb74cc6489f
jimboy3000 <jimboy3100@yahoo.com>
2018-03-09 23:21:08 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -7,93 +7,15 @@ if (!window.AgarToolLoaded) {
             }, 1e3)
         }, document.head.appendChild(e)
     }
-	(function(w){
-	"use strict";
-	/* exported loadCSS */
-	var loadCSS = function( href, before, media ){
-		// Arguments explained:
-		// `href` [REQUIRED] is the URL for your CSS file.
-		// `before` [OPTIONAL] is the element the script should use as a reference for injecting our stylesheet <link> before
-		// By default, loadCSS attempts to inject the link after the last stylesheet or script in the DOM. However, you might desire a more specific location in your document.
-		// `media` [OPTIONAL] is the media type or query of the stylesheet. By default it will be 'all'
-		var doc = w.document;
-		var ss = doc.createElement( "link" );
-		var ref;
-		if( before ){
-			ref = before;
-		}
-		else {
-			var refs = ( doc.body || doc.getElementsByTagName( "head" )[ 0 ] ).childNodes;
-			ref = refs[ refs.length - 1];
-		}
-
-		var sheets = doc.styleSheets;
-		ss.rel = "stylesheet";
-		ss.href = href;
-		// temporarily set media to something inapplicable to ensure it'll fetch without blocking render
-		ss.media = "only x";
-
-		// wait until body is defined before injecting link. This ensures a non-blocking load in IE11.
-		function ready( cb ){
-			if( doc.body ){
-				return cb();
-			}
-			setTimeout(function(){
-				ready( cb );
-			});
-		}
-		// Inject link
-			// Note: the ternary preserves the existing behavior of "before" argument, but we could choose to change the argument to "after" in a later release and standardize on ref.nextSibling for all refs
-			// Note: `insertBefore` is used instead of `appendChild`, for safety re: http://www.paulirish.com/2011/surefire-dom-element-insertion/
-		ready( function(){
-			ref.parentNode.insertBefore( ss, ( before ? ref : ref.nextSibling ) );
-		});
-		// A method (exposed on return object for external use) that mimics onload by polling document.styleSheets until it includes the new sheet.
-		var onloadcssdefined = function( cb ){
-			var resolvedHref = ss.href;
-			var i = sheets.length;
-			while( i-- ){
-				if( sheets[ i ].href === resolvedHref ){
-					return cb();
-				}
-			}
-			setTimeout(function() {
-				onloadcssdefined( cb );
-			});
-		};
-
-		function loadCB(){
-			if( ss.addEventListener ){
-				ss.removeEventListener( "load", loadCB );
-			}
-			ss.media = media || "all";
-		}
-
-		// once loaded, set link's media back to `all` so that the stylesheet applies once it loads
-		if( ss.addEventListener ){
-			ss.addEventListener( "load", loadCB);
-		}
-		ss.onloadcssdefined = onloadcssdefined;
-		onloadcssdefined( loadCB );
-		return ss;
-	};
-	// commonjs
-	if( typeof exports !== "undefined" ){
-		exports.loadCSS = loadCSS;
-	}
-	else {
-		w.loadCSS = loadCSS;
-	}
-	}( typeof global !== "undefined" ? global : this ));
 
     function detectExtensions() {
         try {
-            if ( window.startInfinity || window.raga || document.getElementById("minions") || document.getElementById("minionscomUI2") || document.getElementById("btn-dc-go")) window.location = "http://www.legendmod.ml";
+            if (window.ogario || window.startInfinity || window.raga || document.getElementById("minions") || document.getElementById("minionscomUI2") || document.getElementById("btn-dc-go")) window.location = "https://www.agartool.io/?integrity";
             else {
                 var o = document.getElementsByTagName("script");
                 for (var t in o) {
                     var e = o[t].src;
-                    e && (e.includes("agarinfinity.com") || e.includes("imasters.org.ru")) && (window.location = "http://www.legendmod.ml")
+                    e && (e.includes("ogario.ovh") || e.includes("agarinfinity.com") || e.includes("imasters.org.ru")) && (window.location = "https://www.agartool.io/?integrity")
                 }
             }
         } catch (o) {
@@ -106,14 +28,6 @@ if (!window.AgarToolLoaded) {
             myInterval = setInterval(function() {
                 dispatchEvent(new Event("load")), times++, detectExtensions(), times > 400 && clearInterval(myInterval)
             }, 50);
-		loadCSS( "https://jimboy3100.github.io/bootstrap-colorpicker.min.css" );
-		loadCSS( "https://jimboy3100.github.io/toastr.min.cs" );
-		loadCSS( "https://jimboy3100.github.io/switchery.min.css" );
-		loadCSS( "https://jimboy3100.github.io/rangeslider.css" );
-		loadCSS( "https://jimboy3100.github.io/perfect-scrollbar.min.css" );
-		loadCSS( "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" );
-		loadCSS( "http://cdn.ogario.ovh/v3/ogario.v3.css?v=339" );
-		
         loadScript("http://cdn.agartool.io/" + AgarToolCDNFolder + "/version.js?ts=" + Date.now(), function() {
             loadScript("http://cdn.agartool.io/" + AgarToolCDNFolder + "/html" + AgarToolHTMLVersion + ".js", function() {
                 document.documentElement.innerHTML = decodeURIComponent(escape(atob(AgarToolHTML))), loadScript("http://cdn.agartool.io/jquery-3.2.0.min.js", function() {
