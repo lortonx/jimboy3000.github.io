fe8eab4ee63e2dd19556fa947e9ff408523eb8d3
jimboy3000 <jimboy3100@yahoo.com>
2018-11-05 03:28:16 +0200

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,8 +1,8 @@
 /*************
- * Legend express v0.048 by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v0.049 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
  
-var semimodVersion = "48"; // the version 1.1-> 1.11
+var semimodVersion = "49"; // the version 1.1-> 1.11
 //fix ffa
 /*
 setTimeout(function() {
@@ -521,7 +521,7 @@ function init(modVersion) {
         //							'<button id="YoutubeAutoBtn" type="button" class="btn btn-block btn-info" data-toggle="button" aria-pressed="false" autocomplete="off" style="margin-top: 2px;"><i class="fa fa-youtube-play"></i>Auto Youtube On</button></div>');
         //	$('#music').append(savemusic+'</div>');						
 
-/*
+
         if (typeof YT !== 'undefined') {
             musicPlayer = new YT.Player('musicFrame', {
                 events: {
@@ -537,7 +537,7 @@ function init(modVersion) {
                 }
             });
         }
-*/		
+		
         //roller and other icons
         $('#exp-bar > .icon-user').addClass('fa fa-cog fa-spin fa-3x fa-fw').removeClass('icon-user');
         //$('.music-tab').children().addClass('quick-yt icon-youtube2').removeClass('icon-music').attr('data-original-title', 'Youtube & Sounds');
