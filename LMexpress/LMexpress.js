8b711bf019160275dd22aa3ab3efde7cfd43068d
jimboy3000 <jimboy3100@yahoo.com>
2018-11-13 07:28:08 +0200

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,5 +1,5 @@
 /*************
- * Legend express v0.061 by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v0.051 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
  
 var semimodVersion = "51"; // the version 1.1-> 1.11
@@ -544,7 +544,7 @@ function init(modVersion) {
         //							'<button id="YoutubeAutoBtn" type="button" class="btn btn-block btn-info" data-toggle="button" aria-pressed="false" autocomplete="off" style="margin-top: 2px;"><i class="fa fa-youtube-play"></i>Auto Youtube On</button></div>');
         //	$('#music').append(savemusic+'</div>');						
 
-/*
+
         if (typeof YT !== 'undefined') {
             musicPlayer = new YT.Player('musicFrame', {
                 events: {
@@ -560,7 +560,7 @@ function init(modVersion) {
                 }
             });
         }
-*/
+
         // player shortcut
 		
         $("#playerBtn").click(function() {           
