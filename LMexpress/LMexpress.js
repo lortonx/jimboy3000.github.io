579996eeb861763397913aa3d5bdf3f41d040f9c
jimboy3000 <jimboy3100@yahoo.com>
2018-12-04 02:31:54 +0200

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,8 +1,8 @@
 /**************
- * Legend express v0.073 by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v0.074 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
  
-var semimodVersion = "73"; // the version 1.1-> 1.11
+var semimodVersion = "74"; // the version 1.1-> 1.11
 //fix ffa
 /*
 setTimeout(function() {
@@ -661,7 +661,8 @@ function init(modVersion) {
                 showSearchHud();
                 $("#searchInput").val(pastedData);
                 $("#searchInput").select();
-                searchHandler(pastedData);
+                //searchHandler(pastedData);
+				$("#searchBtn").click();
             }
         });
 
@@ -670,7 +671,8 @@ function init(modVersion) {
                 var pastedData = e.originalEvent.clipboardData.getData('text');
                 $("#searchInput").val(pastedData);
                 $("#searchInput").select();
-                searchHandler(pastedData);
+                //searchHandler(pastedData);
+				$("#searchBtn").click();
             }
         });
 
@@ -1023,8 +1025,10 @@ function init(modVersion) {
         });
 
         $("#searchBtn").click(function() {
-            var searchString = $("#searchInput").val();
-            searchHandler(searchString);
+			getSNEZServers();
+			client2.connect();
+            //var searchString = $("#searchInput").val();
+            //searchHandler(searchString);
         });		
         $("#searchInput").keyup(function(event) {
             if (event.keyCode == 13) {
@@ -2236,6 +2240,8 @@ function changeServer() {
     adres();
     appendLog($("#leaderboard-positions").text());
 }
+
+
 
 function isValidIpAndPort(input) {
     var parts = input.split(":");
@@ -4589,6 +4595,7 @@ function hideSearchHud() {
     $("#searchLog").fadeOut();
 }
 function appendLog(message) {
+	$("#logTitle").text("Leaderboard history");
     var region = $("#gamemode").val();
     $("#log").prepend('<p style="display: none;white-space: nowrap;margin-bottom: 10px;">' +
         '<span class="main-color">' + region.substring(0, 2) + '</span> &nbsp;' +
@@ -4596,6 +4603,20 @@ function appendLog(message) {
 
     $("#log p").first().show(100);
     bumpLog();
+}
+function appendLog2(message, message2) {
+	$("#logTitle").text("Legend mod users (click and join)");
+    var region = $("#gamemode").val();
+    $("#log").prepend('<p style="display: none;white-space: nowrap;margin-bottom: 10px;">' +
+        '<span class="main-color">' + region.substring(0, 2) + '</span> &nbsp;' +
+        '<a onclick="connectto(\`'+message2+'\`);return false;" class="logEntry" data-token="' + currentToken + '" style="color: lightgrey; font-size: 14px;">' + message + '</a></p>');
+
+    $("#log p").first().show(100);
+    bumpLog();
+}
+function connectto(message2){
+				$('#server-token').val(message2);
+				$('#server-join').click();
 }
 
 function bumpLog() {
@@ -6588,7 +6609,7 @@ var socket = {
     },
     updateServerDetails: function()
     {
-        console.log("Details have changed");
+//        console.log("Details have changed");
 //        console.log(state);
 
         socket.send({
@@ -6691,4 +6712,155 @@ function getCookie(cname)
     onUILoaded(initLc, null);
 })();
 	
+}
+
+function getSNEZServers(){
+var client2 = {
+
+    // Properties
+    server: "ws://lc.snez.org:3050/",
+    ws: null,
+    isOpen: false,
+    onOpenCallback: null,
+    onCloseCallback: null,
+    onMessageCallback: null,
+
+    // Methods
+    connect: function()
+    {
+        client2.ws = new WebSocket(client2.server);
+        client2.ws.onopen = client2.onOpen;
+        client2.ws.onclose = client2.onClose;
+        client2.ws.onmessage = client2.onMessage;
+		
+    },
+	disconnect: function()
+	{
+   // Close the connection, if open.
+
+      client2.ws.close();
+
+	},
+    onOpen: function()
+    {
+        console.log("Connected.");
+ //       app.state = "Connected.";
+    },
+
+    onClose: function()
+    {
+//       console.log("Reconnecting in 5 seconds...");
+//        setTimeout(client2.connect, 5000);
+    },
+
+    onMessage: function(evt)
+    {
+        if (client2.isEmpty(evt))
+            return;
+
+        try
+        {
+            var data = JSON.parse(evt.data);
+
+            if (client2.isEmpty(data) || client2.isEmpty(data.type))
+                return;
+        }
+        catch (e)
+        {
+            console.log(e);
+            return;
+        }
+
+        switch (data.type)
+        {
+            case "ping":
+                client2.send({ type: "pong" });
+                break;
+            case "players_list":
+                client2.updatePlayers(data.data);
+                break;
+        }
+    },
+
+    isEmpty: function(obj)
+    {
+        if (typeof obj == 'undefined')
+            return true;
+
+        // For arrays or empty strings
+        if (obj.length === 0)
+            return true;
+
+        // For objects
+        for(var key in obj)
+            if (obj.hasOwnProperty(key))
+                return false;
+
+        return true;
+    },
+
+    updatePlayers: function(data)
+    {
+		var showonceusers=0;
+		var showonceusers2=0;
+		for (var player in data) {
+  			if (data.hasOwnProperty(player)) {
+				if (data[player].nickname){
+    			if (data[player].nickname.indexOf($("#searchInput").val()) >= 0) {
+					if (showonceusers==0){
+					showonceusers++;
+					toastr["info"]("User Found. Revealing server...");
+					}
+					var temporaryserver=JSON.stringify(data[player]);
+					var temporaryserver2;
+					var temporaryserver3;
+					temporaryserver = temporaryserver.substring(0, temporaryserver.indexOf('.agar.io'));
+					temporaryserver2 = temporaryserver.split('live-arena-').pop();
+					temporaryserver3 = temporaryserver.split('nickname\"\:\"').pop();
+					temporaryserver3 =temporaryserver3.substring(temporaryserver3, temporaryserver3.indexOf('\"\,\"server'));
+					//temporaryserver.substring(0, temporaryserver.indexOf(':'));
+					appendLog2(temporaryserver3 + " (" + temporaryserver2 + ")", temporaryserver2);
+					//JSON.stringify(data[player]);
+					//console.log(data[player]);
+					
+    			}
+    			if (data[player].server.indexOf($("#searchInput").val()) >= 0) {
+					if (showonceusers2==0){
+					showonceusers2++;					
+					toastr["info"]("Server Found. Revealing users...");
+					}
+					var temporaryserver=JSON.stringify(data[player]);
+					var temporaryserver2;
+					var temporaryserver3;
+					temporaryserver = temporaryserver.substring(0, temporaryserver.indexOf('.agar.io'));
+					temporaryserver2 = temporaryserver.split('live-arena-').pop();
+					temporaryserver3 = temporaryserver.split('nickname\"\:\"').pop();
+					temporaryserver3 =temporaryserver3.substring(temporaryserver3, temporaryserver3.indexOf('\"\,\"server'));
+					//temporaryserver.substring(0, temporaryserver.indexOf(':'));
+					appendLog2(temporaryserver3 + " (" + temporaryserver2 + ")", temporaryserver2);
+					//JSON.stringify(data[player]);
+					//console.log(data[player]);
+					
+    			}					
+				}
+				else{
+		            toastr["warning"]('Server / Leaderboard, not found. Do you want the 1-by-1 manual search leaderboards of <font color="yellow">' + $("#region").val() + ' / ' + $("#gamemode").val() + '</font> ?' + '</br> <button id= "manualsearch" class="btn btn-block btn-info" style="margin-top: 10px;border-color: darkblue;">' + Premadeletter24 + '</button><br><button class="btn btn-sm btn-warning btn-exit" style="width: 100%;margin-top: -20px;">' + Premadeletter25 + '</button>', "", {
+		                timeOut: 20000,
+		                extendedTimeOut: 20000
+		            }).css("width", "350px");
+		            $("#manualsearch").click(function() {
+            var searchString = $("#searchInput").val();
+            searchHandler(searchString);	                
+		            });
+				}
+  			}
+		}
+		client2.ws.close();	
+    },
+
+    send: function(data)
+    {
+        client2.ws.send(JSON.stringify(data));
+    }
+};
 }
