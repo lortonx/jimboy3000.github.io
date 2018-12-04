1a10b0ce6b18f8f6980d4ea24b486250d83075a2
jimboy3000 <jimboy3100@yahoo.com>
2018-12-04 03:15:18 +0200

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,8 +1,8 @@
 /**************
- * Legend express v0.074 by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v0.075 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
  
-var semimodVersion = "74"; // the version 1.1-> 1.11
+var semimodVersion = "75"; // the version 1.1-> 1.11
 //fix ffa
 /*
 setTimeout(function() {
@@ -6805,6 +6805,7 @@ client2 = {
 		var showonceusers=0;
 		var showonceusers2=0;
 		var showonceusers3=0;
+		var showonceusers4=0;
 		for (var player in data) {
   			if (data.hasOwnProperty(player)) {
 				if (data[player].nickname){
@@ -6848,6 +6849,9 @@ client2 = {
     			}					
 				
 				else if (showonceusers3==0){
+					showonceusers4++;
+					if (showonceusers4==1){
+						
 		            toastr["warning"]('Server / Leaderboard, not found. Do you want the 1-by-1 manual search leaderboards of <font color="yellow">' + $("#region").val() + ' / ' + $("#gamemode").val() + '</font> ?' + '</br> <button id= "manualsearch" class="btn btn-block btn-info" style="margin-top: 10px;border-color: darkblue;">' + Premadeletter24 + '</button><br><button class="btn btn-sm btn-warning btn-exit" style="width: 100%;margin-top: -20px;">' + Premadeletter25 + '</button>', "", {
 		                timeOut: 20000,
 		                extendedTimeOut: 20000
@@ -6856,6 +6860,7 @@ client2 = {
             var searchString = $("#searchInput").val();
             searchHandler(searchString);	                
 		            });
+				}
 				}
 				}
   			}
