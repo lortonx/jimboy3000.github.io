ac9b35a2aaf39431b5e0d7dc5582070d075d21cd
jimboy3000 <jimboy3100@yahoo.com>
2018-12-12 18:57:39 +0200

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,8 +1,8 @@
 /**************
- * Legend express v0.085 by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v0.086 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
  
-var semimodVersion = "85"; // the version 1.1-> 1.11
+var semimodVersion = "86"; // the version 1.1-> 1.11
 //fix ffa
 /*
 setTimeout(function() {
@@ -47,6 +47,7 @@ var CutNameConflictwithMessage=false;
     };
 })();
 */
+var message3, message4;
 $("#gamemode").prop('disabled', false);
 $("#region").prop('disabled', false);   		
 var oldgamemode=$("#gamemode");
@@ -3277,7 +3278,6 @@ function appendLog2(message, message2) {
 }*/
 function appendLog2(message, message2) {
 	$("#logTitle").text("Legend mod users (click and join)");
-    var region = $("#region").val();
     $("#log").prepend('<p style="display: none;white-space: nowrap;margin-bottom: 10px;">' +
 //        '<span class="main-color">' + region.substring(0, 2) + '</span> &nbsp;' +
         '<a onclick="connectto(\`'+message2+'\`);return false;" class="logEntry" data-token="' + currentToken + '" style="color: lightgrey; font-size: 14px;">' + message + '</a></p>');
@@ -3287,19 +3287,19 @@ function appendLog2(message, message2) {
 }
 function appendLog3(message, message2, message3, message4) {
 	$("#logTitle").text("Legend mod users (click and join)");
-    var region = $("#region").val();
     $("#log").prepend('<p style="display: none;white-space: nowrap;margin-bottom: 10px;">' +
 //        '<span class="main-color">' + region.substring(0, 2) + '</span> &nbsp;' +
-        '<a onclick="connectto(\`'+message2+'\`);connectto2(message3, message4);return false;" class="logEntry" data-token="' + currentToken + '" style="color: lightgrey; font-size: 14px;">' + message + '</a></p>');
-
+        '<a onclick="connectto(\`'+message2+'\`);connectto2();return false;" class="logEntry" data-token="' + currentToken + '" style="color: lightgrey; font-size: 14px;">' + message + '</a></p>');
     $("#log p").first().show(100);
     bumpLog();
+	return message3, message4;
 }
 function connectto(message2){
 				$('#server-token').val(message2);
 				$('#server-join').click();
 }
-function connectto2(message3, message4){
+
+function connectto2(){
 		setTimeout(function () {
 			$('#gamemode').val(message3);
 			$("#region").val(message4);			
