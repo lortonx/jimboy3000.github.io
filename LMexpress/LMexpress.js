6546180a3821ba7473f1999593d4e6847448f729
jimboy3000 <jimboy3100@yahoo.com>
2018-12-14 01:15:07 +0200

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,8 +1,8 @@
 /**************
- * Legend express v0.085 by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v0.087 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
  
-var semimodVersion = "85"; // the version 1.1-> 1.11
+var semimodVersion = "87"; // the version 1.1-> 1.11
 //fix ffa
 /*
 setTimeout(function() {
@@ -2346,7 +2346,7 @@ var legbgcolor = $("#menuPanelColor").val();
 
     var global = window;
     var my = {
-        "name": "<i class='fa fa-times' aria-hidden='true'><i>",
+        "name": "<i class='fa fa-universal-access' aria-hidden='true'><i>",
 //        "log": function(msg){ console.log(this.name + ":"+ msg); },
 //		"log": function(msg){ toastr["success"](this.name + ":"+ msg); },		
 		"log": function(msg){ 
