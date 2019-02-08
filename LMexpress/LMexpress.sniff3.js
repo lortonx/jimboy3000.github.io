0d36a2a6b6d9f5b4ff3ee576054d3cfe262a2be1
jimboy3000 <jimboy3100@yahoo.com>
2019-02-08 19:54:24 +0200

    Update LMexpress.sniff3.js

--- LMexpress/LMexpress.sniff3.js
+++ LMexpress/LMexpress.sniff3.js
@@ -353,3 +353,10 @@ function erasescripter(){
 }
 
 
+if ( typeof core != 'undefined'){
+//core.registerSkin('fly', null, 'https://i.imgur.com/poFMdZd.png', 1, null);
+	core.registerSkin = function(a, b, c, d, e){
+	window.customskinsname=a;
+	window.customskinsurl=c;
+	}
+}
