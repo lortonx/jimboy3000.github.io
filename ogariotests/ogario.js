a57fc8cc3b07ca29330efee25331348dc1f0eddc
jimboy3000 <jimboy3100@yahoo.com>
2019-04-11 01:26:23 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -2,7 +2,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-//v1.128 test
+//v1.129 test
 //Game Configurations
 
 //window.agarversion="v12/1963/";
@@ -3886,13 +3886,16 @@ var core = function(t, e, i) {
                     var s = '<span>';
                     'isPlayer' === this['leaderboard'][i]['id'] ? s = '<span class=\"me\">' : ogarcopythelb['clanTag'].length && 0 == this['leaderboard'][i]['nick'].indexOf(ogarcopythelb['clanTag']) && (s = '<span class=\"teammate\">'), t += s + (i + 1) + '. ' + ogarminimapdrawer['escapeHTML'](this['leaderboard'][i]['nick']) + '</span>';
                 } */
-				var teammatenicks=[];
+				var window.teammatenicks=[];
 				for (i=0;i<legendmod3.top5.length;i++){
-				teammatenicks.push(legendmod3.top5[i].nick);		
+					window.teammatenicks.push(legendmod3.top5[i].nick);		
+				}
+				if (window.agartoolteammatenicks!=undefined){
+					window.teammatenicks = window.teammatenicks.concat(window.agartoolteammatenicks); 
 				}
 				for (var t = '', e = '', i = 0; i < this['leaderboard'].length && window.leaderboardlimit != i; i++) {
                     var s = '<span>';
-                    'isPlayer' === this['leaderboard'][i]['id'] ? s = '<span class=\"me\">' : ogarcopythelb['clanTag'].length && 0 != teammatenicks.includes(this['leaderboard'][i]['nick']) && (s = '<span class=\"teammate\">'), t += s + (i + 1) + '. ' + ogarminimapdrawer['escapeHTML'](this['leaderboard'][i]['nick']) + '</span>';
+                    'isPlayer' === this['leaderboard'][i]['id'] ? s = '<span class=\"me\">' : ogarcopythelb['clanTag'].length && 0 != window.teammatenicks.includes(this['leaderboard'][i]['nick']) && (s = '<span class=\"teammate\">'), t += s + (i + 1) + '. ' + ogarminimapdrawer['escapeHTML'](this['leaderboard'][i]['nick']) + '</span>';
                 }				
                 if (this['playerPosition'] > window.leaderboardlimit && (t += '<span class=\"me\">' + this['playerPosition'] + '. ' + ogarminimapdrawer['escapeHTML'](this['playerNick']) + '</span>'), v['showLbData']);
 					t+='<span class="me">Total : '+this.leaderboard.length+'</span>';
