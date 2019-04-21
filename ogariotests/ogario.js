95942a15180eddfb4ea3f5c2af1879689371426e
jimboy3000 <jimboy3100@yahoo.com>
2019-04-21 18:45:14 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -2,7 +2,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-//v1.170 test
+//v1.171 test
 //Game Configurations
 
 window.agarversion="v12/2106/";
@@ -2121,8 +2121,8 @@ var core = function(t, e, i) {
             for (; entityType < PL$29; entityType++) {
               bufferString = bufferString + this["top5"][entityType]["mass"];
               if (!(entityType >= v["limTP"])) {
-                pix_color = pix_color + ('<li id="player"><span id="pos-skin" style="background-color: ' + this["top5"][entityType]["color"] + '; width: 30px; height:30px; display: inline-block;"><img src = ' + (this["top5"][entityType]["skin"] ? this["top5"][entityType]["skin"] : "https://jimboy3100.github.io/banners/icon32croped.ico.gif") + '" alt=""> ' + 
-				this["escapeHTML"](this["top5"][entityType]["nick"]) + '</span><span id="top5marker" class=""> ' + this["calculateMapSector"](this["top5"][entityType]["x"], this["top5"][entityType]["y"]) + '</span><span id= "top5mass" class=""> ' + 
+                pix_color = pix_color + ('<li id="player"><span id="pos-skin" style="background-color: ' + this["top5"][entityType]["color"] + '; width: 30px; height:30px; display: inline-block;"><img src = ' + (this["top5"][entityType]["skin"] ? this["top5"][entityType]["skin"] : "https://jimboy3100.github.io/banners/CropedImage128.gif") + '" alt=""> ' + 
+				this["escapeHTML"](this["top5"][entityType]["nick"]) + '</span><span id="top5marker" class=""> ' + this["calculateMapSector"](this["top5"][entityType]["x"], this["top5"][entityType]["y"]) + '</span><span id= "top5mass" class=""> style="background-color:' + this["top5"][entityType]["color"] +
                 this["shortMassFormat"](this["top5"][entityType]["mass"]) + '</span></li>');
               }
             }
