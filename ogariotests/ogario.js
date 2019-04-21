e4902cd00935d69fbea2b638a6a893435fba2a5f
jimboy3000 <jimboy3100@yahoo.com>
2019-04-21 16:39:03 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -2,7 +2,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-//v1.158 test
+//v1.159 test
 //Game Configurations
 
 window.agarversion="v12/2106/";
@@ -1436,7 +1436,7 @@ var core = function(t, e, i) {
 				////
 				"color" : "#fff",
 				//"commanderImage" : "https://i.imgur.com/wQKUDB3.png",				
-				"commanderImage" : "https://jimboy3100.github.io/banners/CropedImage128.gif",
+				"commanderImage" : "https://jimboy3100.github.io/banners/CropedImage200.gif",
 				"commanderImage1" : "https://i.imgur.com/XODc5iZ.png",
 				"commanderImage2" : "https://i.imgur.com/Ux8Pt9I.png",				
 				////
