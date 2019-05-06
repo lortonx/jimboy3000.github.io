70d5e6bc285acd9b4b44d7c21922e76c520e2862
jimboy3000 <jimboy3100@yahoo.com>
2019-05-06 06:40:41 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.156 MEGA TEST
+// v1.157 MEGA TEST
 // Game Configurations
 
 window.agarversion = "v12/2106/";
@@ -5459,8 +5459,8 @@ var core = function(t, e, i) {
 						//this.drawSplitRange(this.ctx, M.biggerSTECellsCache, M.playerCells, M.selectBiggestCell);
 						
                         var a = s ? i.length - 1 : 0;
-						console.log(i[s]['size']);
-						if (i[s]['size']>=400){
+						console.log(i[a]['size']);
+						if (i[a]['size']>=400){
                         t['lineWidth'] = 6, t['globalAlpha'] = g['darkTheme'] ? 0.7 : 0.35, t['strokeStyle'] = g['splitRangeColor'], t['beginPath'](), t.arc(i[a]['x'], i[a]['y'], 1.5*i[a]['size'] + 760, 0, this.pi2, false), t['closePath'](), t['stroke']();
 						}
 				   }
