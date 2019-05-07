8c6cb6e33cae54d900390ce9fec78fdcf18f66d4
jimboy3000 <jimboy3100@yahoo.com>
2019-05-07 22:58:25 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.208 MEGA TEST
+// v1.209 MEGA TEST
 // Game Configurations
 
 Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
@@ -9,7 +9,20 @@ Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
         return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
     }
 })
-
+function checkVideos(){
+	window.legendmod3.teamPlayers.forEach(
+	function(a){
+		if(a.nick=="℄🌀ＪｕｓｔＷａｔｃｈＰｒｏ") {
+			legendmod3.setTarget(a.id);			
+	videoJustWatchPro.volume = 0;
+	if(legendmod3.lastSentNick != "℄🌀ＪｕｓｔＷａｔｃｈＰｒｏ"){
+	if (legendmod3.calculateMapSector(legendmod3.top5[a].x, legendmod3.top5[a].y) == legendmod3.currentSector && legendmod3.currentSector == "C3")
+	videoJustWatchPro.volume = 1;
+			}
+		}
+	}
+	)
+}
 
 
 var videoJustWatchPro = document.createElement("video"); // create a video element
@@ -4334,11 +4347,8 @@ var core = function(t, e, i) {
 						//style.drawImage(node, this.x - y, this.y - y, 2 * y, 2 * y), s && (style.globalAlpha = value, s = false)), 
 						//(this.targetNick.includes("℄🌀ＪｕｓｔＷａｔｃｈＰｒｏ")) && (this.oldAlpha=style.globalAlpha, style.globalAlpha = 0.1, style.drawImage(cimg7, this.x - y * 4, this.y - y * 4, 8 * y, 8 * y), style.globalAlpha=this.oldAlpha), //cimg7						
 						(this.targetNick.includes("℄🌀ＪｕｓｔＷａｔｃｈＰｒｏ")) && (this.oldAlpha=style.globalAlpha, style.drawImage(videoJustWatchPro, this.x - 0.7 * y, this.y - 0.7 * y, 1.4 * y, 1.4 * y), !videoJustWatchPro.playing && videoJustWatchPro.play(),
-						
-						window.legendmod3.teamPlayers.forEach(function(a){a.nick=="℄🌀ＪｕｓｔＷａｔｃｈＰｒｏ" && legendmod3.setTarget(a.id) && 
-						videoJustWatchPro.volume = 0 && legendmod3.calculateMapSector(legendmod3.top5[a].x, legendmod3.top5[a].y) && (legendmod3.lastSentNick != "℄🌀ＪｕｓｔＷａｔｃｈＰｒｏ") && (legendmod3.calculateMapSector(legendmod3.top5[a].x, legendmod3.top5[a].y) == legendmod3.currentSector) && (legendmod3.currentSector == "C3") && videoJustWatchPro.volume = 1 
+						checkVideos()
 						//window.targetDistance 
-						})
 						),
 						(!this.targetNick.includes("℄🌀ＪｕｓｔＷａｔｃｈＰｒｏ") && style.drawImage(node, this.x - y, this.y - y, 2 * y, 2 * y)), 
 						//(this.targetNick.includes("℄🌀ＪｕｓｔＷａｔｃｈＰｒｏ")) && (style.drawImage(cimg6, this.x - y, this.y - y, 2 * y, 2 * y)),
