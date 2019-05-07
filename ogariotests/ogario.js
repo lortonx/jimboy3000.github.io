027dad1075ef0e7b0b2d0ad25446748f1ca919fc
jimboy3000 <jimboy3100@yahoo.com>
2019-05-07 21:08:54 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.192 MEGA TEST
+// v1.193 MEGA TEST
 // Game Configurations
 
 Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
@@ -37,6 +37,7 @@ function myTimer() {
 	}}
 	else {
 		if (videoJustWatchPro.playing) {
+			window.checkVidSkinStat=false;
 			videoJustWatchPro.pause();
 		myStopFunction();
 		window.checkVidSkinStatFlag=false;
