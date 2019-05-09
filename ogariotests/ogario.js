a458626240e4ba419eb2fb4fe294cf2924942649
jimboy3000 <jimboy3100@yahoo.com>
2019-05-10 02:48:29 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.244 MEGA TEST
+// v1.245 MEGA TEST
 // Game Configurations
 
 Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
@@ -2475,11 +2475,17 @@ var core = function(t, e, i) {
             },
             'setSkinPreview': function(t, e) {
                 if (s('#' + e)['empty']().addClass('default'), t && 0 != t.length) {
-                    var i = this; 
+                    if (t.includes(".mp4"){
+					var i = this, 
+                        o = new Image(); console.log(o+":"+t+":"+e);						
+					};
+					else{
+					var i = this, 
                         o = new Image(); console.log(o+":"+t+":"+e);
-                    o.crossOrigin = 'anonymous'; o.onload = function() {
+				}
+                    o.crossOrigin = 'anonymous', o.onload = function() {
                         i['changeSkinPreview'](o, e);
-                    }; o.src = t;
+                    }, o.src = t;
                 }
             },
             'setProfile': function() {
