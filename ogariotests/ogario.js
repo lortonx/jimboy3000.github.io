5360768683fdee4ac0dbea76aacc9f72e188ab70
jimboy3000 <jimboy3100@yahoo.com>
2019-05-11 23:20:50 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.278 MEGA TEST
+// v1.279 MEGA TEST
 // Game Configurations
 
 Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
@@ -2536,7 +2536,7 @@ var core = function(t, e, i) {
 								
                 if (s('#' + e).empty().addClass('default'), t && 0 != t.length) {
 					console.log("stage 1 images/videos: " + t);	
-					var i = this; 
+					var i = this, 
 					o = checktypeImgVid; o.src = t;
                        // o = new Image(); 					
                     o.crossOrigin = 'anonymous', o.onload = function() {
