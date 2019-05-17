ef34b155562fed01af26b6023f036c88e6b90fd4
jimboy3000 <jimboy3100@yahoo.com>
2019-05-18 01:38:32 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.458 MEGA TEST
+// v1.459 MEGA TEST
 // Game Configurations
 
 Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
@@ -3306,14 +3306,14 @@ var core = function(t, e, i) {
 						var fixminidaxx = u-(minidaxx/2);
 						if (fixminidaxx<0){ fixminidaxx=0 }
 						if (fixminidaxx+minidaxx>this.miniMap.width){ 
-						fixminidaxx = fixminidaxx - (this.miniMap.width - minidaxx)/2;
 						minidaxx = minidaxx + (this.miniMap.width - fixminidaxx)/2;
+						fixminidaxx = fixminidaxx - (this.miniMap.width - minidaxx)/2;					
 						}
 						var fixminidayy = d-(minidayy/2);
 						if (fixminidayy<0){ fixminidayy=0 }	
 						if (fixminidayy+minidayy>this.miniMap.height){ 
-						fixminidayy = fixminidayy - (this.miniMap.height - minidayy)/2;
 						minidayy = minidayy + (this.miniMap.height - fixminidayy)/2;
+						fixminidayy = fixminidayy - (this.miniMap.height - minidayy)/2;						
 						}
 						
 						this.miniMapCtx.rect(fixminidaxx, fixminidayy, minidaxx, minidayy);						
