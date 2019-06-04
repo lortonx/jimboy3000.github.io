ed7a880e942897f4515c4c2cf149850b4bfa103f
jimboy3000 <jimboy3100@yahoo.com>
2019-06-04 03:25:03 +0300

    Create blob.js

---
+++ ogariotests/blob.js
@@ -0,0 +1,17 @@
+var sampleBytes = new Int8Array(testobjects.buffer)
+
+var saveByteArray = (function () {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (data, name) {
+        var blob = new Blob(data, {type: "octet/stream"}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = name;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
+
+saveByteArray([sampleBytes], 'example.txt');
