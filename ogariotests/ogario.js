5c0762faf838439808165242652bdee91f18336a
jimboy3000 <jimboy3100@yahoo.com>
2019-06-07 09:33:10 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.645 MEGA TEST
+// v1.647 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -5525,8 +5525,10 @@ var thelegendmodproject = function(t, e, i) {
 					
 					
                     case 5:
+						window.testobjectsOpcode5 = data;			  			
                         break;
                     case 17:
+						window.testobjectsOpcode17 = data;		
                         this.viewX = data.getFloat32(s, true);
 						s += 4;
 						this.viewY = data.getFloat32(s, true);
@@ -5534,16 +5536,19 @@ var thelegendmodproject = function(t, e, i) {
 						this.scale = data.getFloat32(s, true);
                         break;
                     case 18:
+						window.testobjectsOpcode18 = data;	
                         if (this.protocolKey){ 
 						this.protocolKey = this.shiftKey(this.protocolKey);
 						}
 						this.flushCellsData();
                         break;
                     case 32:
+						window.testobjectsOpcode32 = data;	
                         this.playerCellIDs.push(data.getUint32(s, true));
 						this.play || (this.play = true, ogarminimapdrawer.hideMenu(), this.playerColor = null, ogarminimapdrawer.onPlayerSpawn());
                         break;
                     case 50:
+						window.testobjectsOpcode50 = data;	
                         this.pieChart = [];
                         var a = data.getUint32(s, true);
                         s += 4;
@@ -5551,6 +5556,7 @@ var thelegendmodproject = function(t, e, i) {
                         ogarfooddrawer.drawPieChart();
                         break;
                     case 53:
+						window.testobjectsOpcode53 = data;	
                         if (this['leaderboard'] = [], this.playerPosition = 0, 54 == data.getUint8(0)) {
                             data.getUint16(s, true);
                             s += 2;
@@ -5584,8 +5590,10 @@ var thelegendmodproject = function(t, e, i) {
                         this['handleLeaderboard']();
                         break;
                     case 54:
+						window.testobjectsOpcode54 = data;	
                         break;
                     case 69:
+						window.testobjectsOpcode65 = data;	
                         var u = data.getUint16(s, true);
                         s += 2, this.ghostCells = [];
                         for (n = 0; n < u; n++) {
@@ -5606,6 +5614,7 @@ var thelegendmodproject = function(t, e, i) {
                         }
                         break;
                     case 85:
+						window.testobjectsOpcode85 = data;	
                         console.log('[Legend mod Express] Captcha requested'); if(window.master && window.master['recaptchaRequested']) { window.master['recaptchaRequested']();}
                         break;
 			  case 102:
@@ -5656,6 +5665,7 @@ var thelegendmodproject = function(t, e, i) {
 			  
 
             case 103:
+			  window.testobjectsOpcode103 = data;	
               M["accessTokenSent"] = !![];
               break;			  
 			  /*
@@ -5699,16 +5709,21 @@ var thelegendmodproject = function(t, e, i) {
 
 				
                     case 114:
+						window.testobjectsOpcode114 = data;	
 						break;
                     case 161:
+						window.testobjectsOpcode161 = data;	
                         break;
                     case 176:
+						window.testobjectsOpcode176 = data;	
                         this['battleRoyale'].startTime = data.getUint32(s, true);
                         break;
                     case 177:
+						window.testobjectsOpcode177 = data;	
                         this['battleRoyale'].joined = true;
                         break;
                     case 178:
+						window.testobjectsOpcode178 = data;	
                         this['battleRoyale'].players = data.getUint16(s, true), s += 2;
                         var y = data.getUint16(s, true);
                         s += 2, y || (this['battleRoyale'].state = 0, this['battleRoyale'].joined = false),
@@ -5724,11 +5739,13 @@ var thelegendmodproject = function(t, e, i) {
 						this['battleRoyale'].targetY = data.getInt32(s, true), s += 4, this['battleRoyale'].targetRadius = data.getUint32(s, true));
                         break;
                     case 179:
+						window.testobjectsOpcode179 = data;	
                         y = data.getUint8(s);
 						window.decodeURIComponent(escape(i()));
                         y || window.decodeURIComponent(escape(i()));
                         break;
                     case 180:
+						window.testobjectsOpcode181 = data;	
                         this['battleRoyale'].joined = false;
 						this['battleRoyale'].rank = [];
 						this['battleRoyale'].playerRank = data.getUint32(s, true);
@@ -5745,6 +5762,7 @@ var thelegendmodproject = function(t, e, i) {
                         }
                         break;
             case 226:
+			  window.testobjectsOpcode226 = data;	
               var extraOptions = data["getUint16"](1, !![]);
               data = this["createView"](3);
               data["setUint8"](0, 227);
@@ -5752,6 +5770,7 @@ var thelegendmodproject = function(t, e, i) {
               this["sendMessage"](data);
               break;
                     case 241:
+						window.testobjectsOpcode241 = data;	
                         this.protocolKey = data.getUint32(s, true);
 						console.log('[Legend mod Express] Received protocol key:', this.protocolKey);
                         var irenderfromagario = new Uint8Array(data['buffer'], s += 4);
@@ -5761,10 +5780,12 @@ var thelegendmodproject = function(t, e, i) {
 						}
                         break;
                     case 242:
+						window.testobjectsOpcode242 = data;	
                         this['serverTime'] = 1000 * data.getUint32(s, true);
 						this.serverTimeDiff = Date.now() - this['serverTime'];
                         break;
                     case 255:
+						window.testobjectsOpcode255 = data;	
                         this['handleSubmessage'](data);
                         break;
                     default:
