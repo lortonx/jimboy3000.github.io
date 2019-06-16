a02ca60e5e8867f6f4c495ac3d002db8bba38977
jimboy3000 <jimboy3100@yahoo.com>
2019-06-16 03:22:59 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.708 MEGA TEST
+// v1.709 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -5201,12 +5201,12 @@ var thelegendmodproject = function(t, e, i) {
 				this.mapOffsetFixed= false; 
 				this['leaderboard'] = [];
 				this.ws = t;
-				//this.socket = new WebSocket(t); 
+				this.socket = new WebSocket(t); 
 				//this.socket = new WebSocket(t, 'KF44oU2YSf1E76K35qvxqw=='); 
-				this.socket = new WebSocket(t, {
-				origin: 'https://agar.io',
-				rejectUnauthorized: false
-				});
+//				this.socket = new WebSocket(t, [
+//				origin: 'https://agar.io',
+//				rejectUnauthorized: false
+//				]);
 				this.socket['binaryType'] = 'arraybuffer'; 
 				this.socket['onopen'] = function() {
                     i['onOpen']();
