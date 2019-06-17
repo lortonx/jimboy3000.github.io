976220f8b489524bbb6ba837fda111b5a679e5e0
jimboy3000 <jimboy3100@yahoo.com>
2019-06-17 03:57:27 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.756 MEGA TEST
+// v1.757 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -5382,10 +5382,12 @@ var thelegendmodproject = function(t, e, i) {
 						window.SmallerCellFlag = false; setTimeout(function() {window.SmallerCellFlag = true;}, 1000);
 						$('#pause-hud').text(this.cells[node].nick + " is close and will be eaten by split. X: " + parseInt(targetPlayerCell.x - this.playerX) + " , Y: " + parseInt(targetPlayerCell.y - this.playerY));
 					}
+					if (this.playerCells.length==1){
 					target2.x = this.cells[node].x; target2.y = this.cells[node].y;
 					console.log("Target mass: " + this.cells[node].mass);
 					//target2 = this.cells[node];	
 						doSplit=true;
+					}
 					}
 				}				
                 });
