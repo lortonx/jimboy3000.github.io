574991c3367c681e22336d2189ad481f99676751
jimboy3000 <jimboy3100@yahoo.com>
2019-06-18 02:52:09 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.793 MEGA TEST
+// v1.794 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -5363,10 +5363,10 @@ var thelegendmodproject = function(t, e, i) {
 					if (distancePlayerCell < 130 + PlayerCell.size && this.playerMass >125 && PlayerCell.isVirus) {					                   
 					Object.keys(window.DistanceX).forEach(function(key) {
 						console.log(key, window.DistanceX[key]);
-					}
-					if(){
+					});
+					//if(){
 						
-					}
+					//}
 					if (window.VirusFlag == true){						
 						window.VirusFlag = false; setTimeout(function() {window.VirusFlag = true;}, 1000);
 						$('#pause-hud').html("<font color='" + PlayerCell.color + "'>Virus</font> is close. X: " + parseInt(PlayerCell.x - this.playerX) + " , Y: " + parseInt(PlayerCell.y - this.playerY));
