505e877b828c198f3cfc96b3e07354adac856dc4
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-06-23 21:19:11 +0300

    Update mergetimer.js

--- ogariotests/mergetimer.js
+++ ogariotests/mergetimer.js
@@ -1,28 +1,40 @@
 var Intervalstatistics = setInterval(CellTimer, 1000);	
 function CellTimer() {
 	myCells = [];
-	for (var i = 0; i < window.legendmod.cells.length; i++) {
-//legendmod.cells[i].historyMass->window.cellsId[legendmod.cells[i].id].historyMass
+	for (var i = 0; i < window.legendmod.playerCells.length; i++) {
+//legendmod.cells[i].historyMass->window.playerCellsId[legendmod.cells[i].id].historyMass
 		
-			if (window.cellsId[legendmod.cells[i].id].historyMass[window.legendmod2.fps]>window.cellsId[legendmod.cells[i].id].historyMass[0]*1.4){
+			if (window.playerCellsId && window.legendmod.playerCells[i] && window.legendmod.playerCells[i].id && window.playerCellsId[legendmod.playerCells[i].id] && window.playerCellsId[legendmod.playerCells[i].id].historyMass && 
+			window.playerCellsId[legendmod.playerCells[i].id].historyMass[window.legendmod2.fps]>window.playerCellsId[legendmod.playerCells[i].id].historyMass[0]*1.4){
 				var j=i;
 				var x=0;
-				//setTimeout(function() {					
+					
 					MergeCells(j,x);
-				//}, 1000);
+
 			}
+			else{
+				if(window.playerCellsId && window.legendmod.playerCells[i] && window.legendmod.playerCells[i].id && window.playerCellsId[legendmod.playerCells[i].id]){
+				window.playerCellsId[legendmod.playerCells[i].id].mergeTime=null;
+			}
+			}
+			
 		}
 	}	
 
 function MergeCells(j,x){
 x++;
-window.cellsId[legendmod.cells[j].id].mergeTime=30 + (2.33/100)*window.cellsId[legendmod.cells[j].id].mass-x;
-	console.log(j,x, Math.round(window.cellsId[legendmod.cells[j].id].mergeTime));
-	if (window.cellsId[legendmod.cells[j].id].mergeTime>0){
+if (window.playerCellsId && window.legendmod.playerCells[j] && window.legendmod.playerCells[j].id && window.playerCellsId[legendmod.playerCells[j].id]){
+window.playerCellsId[legendmod.playerCells[j].id].mergeTime = 30 + (2.33/100)*window.playerCellsId[legendmod.playerCells[j].id].historyMass[0] - x;
+	console.log(j,x, Math.round(window.playerCellsId[legendmod.playerCells[j].id].mergeTime));
+	if (window.playerCellsId[legendmod.playerCells[j].id].mergeTime>0){
 	setTimeout(function() {	
 		MergeCells(j,x);
 		}, 1000);
 	}
+	else{
+		window.playerCellsId[legendmod.playerCells[j].id].mergeTime=null;
+	}
+}	
 	return j,x;
 }
 
