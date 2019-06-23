dc3285cdaefe0450fa97c79ec3c852fe7621cd24
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-06-23 20:01:48 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko
 // This is part of the Legend mod project
-// v1.834 MEGA TEST
+// v1.835 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -4723,8 +4723,8 @@ var thelegendmodproject = function(t, e, i) {
                 this.redrawMass = Math.abs((this.mass - this.lastMass) / this.mass) >= 0.02 || this.rescale;				
             }
             //if (this.mergeTime && this.mergeTime > 0) {     
-			if(window.cellsId && window.cellsId[this.id] && window.cellsId[this.id].mergeTime && window.cellsId[this.id].mergeTime > 1 ){
-                this.massTxt = this.massTxt + '[' + window.cellsId[this.id].mergeTime + ']';
+			else if(!v.optimizedMass && window.cellsId && window.cellsId[this.id] && window.cellsId[this.id].mergeTime && window.cellsId[this.id].mergeTime > 1 ){
+                this.massTxt = this.massTxt + '[' + math.round(window.cellsId[this.id].mergeTime) + ']';
             return true;    
             }			
             return true;
