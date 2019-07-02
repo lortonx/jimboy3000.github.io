2f7ddb655f09432a5d1770d03de20e37eb86ae28
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-07-02 03:15:00 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.934 MEGA TEST
+// v1.935 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -3673,7 +3673,9 @@ var thelegendmodproject = function(t, e, i) {
 					if (e.includes(window.EnvConfig.config_url)){
 						e= "https://jimboy3100.github.io/vanillaskins/" + e.split('/').pop(); //if CORS policy on miniclip images, use other source
 						console.log("new destinamtions is:" + e);
+						t[e].onload();
 						return e;
+						
 					}
 					};
 				t[e].src = e;
