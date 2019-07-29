efc54b272bfbdad42808b460bc81a3d8099a6b8e
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-07-29 11:06:11 +0300

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,5 +1,5 @@
 /**************
- * Legend express v0.062s by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v0.061t by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 var semimodVersion = "60"; // the version 1.1-> 1.11
 //fix ffa
@@ -3498,10 +3498,10 @@ function universalchat() {
 			for (var n = 1; n <= i; n++){
 			if (i - n >= 0 && stat.minimapBalls[keys[i]].leaderboardpos < stat.minimapBalls[keys[i-n]].leaderboardpos){
 				console.log(stat.minimapBalls[keys[i]].leaderboardpos + ' position changed with ' + stat.minimapBalls[keys[i-n]].leaderboardpos )
-				var temp = keys[i];				
-				keys[i] = keys[i-n];
-				keys[i-n] = temp;
-				return keys[i], keys[i-n];
+				var temp = stat.minimapBalls[keys[i]];				
+				stat.minimapBalls[keys[i]] = stat.minimapBalls[keys[i-n]];
+				stat.minimapBalls[keys[i-n]] = temp;
+				
 			}
 			}
 			
