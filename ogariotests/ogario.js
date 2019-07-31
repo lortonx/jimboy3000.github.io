c76a3c74b96f5ba061f3a025eac930e881163d4a
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-07-31 12:12:08 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1000 MEGA TEST
+// v1.1001 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -4455,9 +4455,9 @@ var thelegendmodproject = function(t, e, i) {
                      ogariocellssetts.time=this.time;
 					 ogariocellssetts.isVirus=true;
 					 temp.push(ogariocellssetts);					 
-					 if (!this.cells.hasOwnProperty(e)) {
+					 //if (!legendmod.cells.hasOwnProperty(e)) {
 						 legendmod.cells.push(ogariocellssetts);						 
-					 }
+					 //}
 					 ogariocellssetts.removeCell();
                  }
                  this.teamPlayers[id].dvirs = temp;
@@ -4478,9 +4478,9 @@ var thelegendmodproject = function(t, e, i) {
                      var ogariocellssetts = new ogarbasicassembly(di, x, y, ds, null, false, true, false, defaultmapsettings.shortMass, defaultmapsettings.virMassShots);
                      ogariocellssetts.isVirus=false;
 					 temp.push(ogariocellssetts);					 
-					 if (!this.cells.hasOwnProperty(e)) {
+					 //if (!legendmod.cells.hasOwnProperty(e)) {
 						 legendmod.cells.push(ogariocellssetts);						 
-					 }
+					 //}
 					 ogariocellssetts.removeCell();
                  }
                  this.teamPlayers[id].dcells = tempx;
