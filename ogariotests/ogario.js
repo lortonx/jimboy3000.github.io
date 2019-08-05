41011a2a3e09aa9980ffa2a64297faeaa51bfd49
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-08-05 14:08:27 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1044 MEGA TEST
+// v1.1045 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -4503,17 +4503,20 @@ var thelegendmodproject = function(t, e, i) {
                      ogariocellssetts.time=this.time;
 					 ogariocellssetts.isVirus=true;
 					 temp.push(ogariocellssetts);	
-					 //if (!legendmod.indexedCells[id].isInView()){					 
+					 //if (!legendmod.indexedCells[id].isInView()){	
+					var ai=legendmod.cells.length;
 					 if (legendmod.indexedCells.hasOwnProperty(di)) {						 
 						 ogariocellssetts = legendmod.indexedCells[di];
 						 													 
 						 }
 				     else{
 						legendmod.indexedCells[di] = ogariocellssetts; 
-						legendmod.cells.push(ogariocellssetts);
+						legendmod.cells[ai]=ogariocellssetts;
 					 }
 					 //}
-					 ogariocellssetts.removeCell();
+					 setTimeout(function() {
+					 legendmod.cells[ai].removeCell();
+					 }, 1000);
                  }
                  this.teamPlayers[id].dvirs = temp;
                 
