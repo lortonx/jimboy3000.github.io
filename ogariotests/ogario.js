41d8ba1c7e44f5fbbdf1648073f6421bb8dc0ebd
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-08-07 08:32:21 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1070 MEGA TEST
+// v1.1071 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -9089,7 +9089,7 @@ var thelegendmodproject = function(t, e, i) {
         window.core = {
             'connect': function(t) {
                 LM.connect(t);
-                //LM1.connect(t); for multibox with new Protocol and Client
+                LM.connect(t); //for multibox with new Protocol and Client
             },
             'disconnect': function() {},
             'sendNick': function(t) {
