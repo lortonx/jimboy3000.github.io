ce22edd003cdb001354ab9ee6c86aa060e320948
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-08-09 14:50:12 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1093 MEGA TEST
+// v1.1094 MEGA TEST
 // Game Configurations
 //team view
 
@@ -4149,7 +4149,7 @@ var thelegendmodproject = function(t, e, i) {
                     console.log('[Legend mod Express] SLG socket error', e);
                     window.noSLGSocket = true;					
                 };				
-			}
+			},
             'closeConnection': function() {
                 if (this.socket) {
                     this.socket['onmessage'] = null;
