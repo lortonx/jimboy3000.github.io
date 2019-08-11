9cae61bb6e0bdd1a71bd532c30526e3f12348bf6
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-08-11 23:13:17 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1117 MEGA TEST
+// v1.1118 MEGA TEST
 // Game Configurations
 //team view
 
@@ -2741,7 +2741,7 @@ var thelegendmodproject = function(t, e, i) {
 						//
 							//t += '<span class=\"hud-main-color\">[' + this.calculateMapSector(this.top5[o].x, this.top5[o].y) + ']</span>',
 							t += '<span class=\"hud-main-color\">[' + this.calculateMapSector(this.w, this.u) + ']</span>';
-							t += '<span class=\"top5-mass-color\">[' + this.shortMassFormat(this.top5[o].mass) + ']</span> ' + this.escapeHTML(this.top5[o].nick) + '</li>');
+							t += '<span class=\"top5-mass-color\">[' + this.shortMassFormat(this.top5[o].mass) + ']</span> ' + this.escapeHTML(this.top5[o].nick) + '</li>';
 					}
 							this['top5pos'].innerHTML = t, 
 							i.play && i.playerMass && (e += i.playerMass, s++),
