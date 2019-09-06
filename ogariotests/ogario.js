98adccf049d533205b62ce9c2486ca7259193aa1
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-09-06 05:12:52 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1267 MEGA TEST
+// v1.1268 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -3378,11 +3378,11 @@ var thelegendmodproject = function(t, e, i) {
 					<br>
 					<button id="startBots" class="btn btn-primary btn" disabled>Start Bots</button>
 					<button id="stopBots" class="btn btn-danger">Stop Bots</button>
-					<div><span id="handleCaptchaBotsArea" style="display: none"><input type="checkbox" id="handleCaptchaBots"> <b>Handle bots with captcha</b>
+					<div><span id="handleCaptchaBotsArea" style="display: none"><input type="checkbox" id="handleCaptchaBots"></input> <b>Handle bots with captcha</b>
 					<br>
-					<input type="checkbox" id="solveCaptchaBots" disabled> <b>Solve captcha when loosing</b>
+					<input type="checkbox" id="solveCaptchaBots" disabled></input> <b>Solve captcha when loosing</b>
 					<br>
-					<input type="checkbox" id="pushCaptchaBots" disabled> <b>Push <font id="pushCaptchaBotsAmount" color="red">more</font> bots instead of 1</b>					
+					<input type="checkbox" id="pushCaptchaBots" disabled></input> <b>Push <font id="pushCaptchaBotsAmount" color="red">more</font> bots instead of 1</b>					
 					</span></div>					
 					<br><br><br><br><u><a href="https://github.com/jimboy3100/jimboy3100.github.io/tree/master/ExampleScripts/agario-bots2" target="_blank">Installation</a></u>	
 					<u><a href="https://repl.it/@jimboy3100/agario-bots" target="_blank">repl.it VPS</a></u>
@@ -9719,41 +9719,31 @@ function setGUIEvents() {
 			}		
 		}
     })
-    document.getElementById('stopBots').addEventListener('click', () => {
-        if (window.userBots.startedBots) window.connectionBots.send(new Uint8Array([1]).buffer)
-    })
-    document.getElementById('handleCaptchaBots').addEventListener('click', () => {
-		if ($('#handleCaptchaBots').is(':checked')) {		
-            window.connectionBots.send(new Uint8Array([11]).buffer)
-        } else if (!$('#handleCaptchaBots').is(':checked')){			
-            window.connectionBots.send(new Uint8Array([12]).buffer)
-        }
-    })
-    document.getElementById('solveCaptchaBots').addEventListener('click', () => {
-		if ($('#solveCaptchaBots').is(':checked')) {		
-            window.connectionBots.send(new Uint8Array([13]).buffer)
-        } else if (!$('#solveCaptchaBots').is(':checked')){		
-            window.connectionBots.send(new Uint8Array([14]).buffer)
-        }
-    })	
-    document.getElementById('pushCaptchaBots').addEventListener('click', () => {
-		if ($('#pushCaptchaBots').is(':checked')) {		
-            window.connectionBots.send(new Uint8Array([15]).buffer)
-        } else if (!$('#pushCaptchaBots').is(':checked')){				
-            window.connectionBots.send(new Uint8Array([16]).buffer)
-        }
-    })	
-/*	
 	$('#handleCaptchaBots').click(function() {        
 		if (this.checked) {
+			window.connectionBots.send(new Uint8Array([11]).buffer)
 			$('#solveCaptchaBots').removeAttr("disabled")
 			$('#pushCaptchaBots').removeAttr("disabled")			
         } else {
+			window.connectionBots.send(new Uint8Array([12]).buffer)
 			$('#solveCaptchaBots').attr("disabled", true);
 			$('#pushCaptchaBots').attr("disabled", true);			
         }
     })
-*/	
+	$('#solveCaptchaBots').click(function() {        
+		if (this.checked) {
+			window.connectionBots.send(new Uint8Array([14]).buffer)		
+        } else {
+			window.connectionBots.send(new Uint8Array([15]).buffer)		
+        }
+    })	
+	$('#pushCaptchaBots').click(function() {        
+		if (this.checked) {
+			window.connectionBots.send(new Uint8Array([15]).buffer)		
+        } else {
+			window.connectionBots.send(new Uint8Array([16]).buffer)		
+        }
+    })		
 }
 
 /*
