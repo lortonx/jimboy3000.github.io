05025dcf4f32d050d1ed18494cc78cb8c91ffb7d
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-09-22 07:08:49 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1299 MEGA TEST
+// v1.1300 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -4151,21 +4151,14 @@ var thelegendmodproject = function(t, e, i) {
                             this.height &&
                             this.width <= 2000 &&
                             this.height <= 2000 &&
-							(!animated &&
                             ((i.cacheQueue.push(e),
                                 1 == i.cacheQueue.length &&
-                                i.cacheSkin(i.customSkinsCache)),
+                                i.cacheSkin(i.customSkinsCache, animated)),
                             (i.cacheQueue2.push(e),
                                 1 == i.cacheQueue2.length &&
-                                i.cacheSkin2(i.customSkinsCache)))):
-                            ((i.cacheQueue.push(e, true),
-                                1 == i.cacheQueue.length &&
-                                i.cacheSkin(i.customSkinsCache)),
-                            (i.cacheQueue2.push(e, true),
-                                1 == i.cacheQueue2.length &&
-                                i.cacheSkin2(i.customSkinsCache)));	
-							)								
-							}
+                                i.cacheSkin2(i.customSkinsCache, animated)));	
+							}							
+							
                     },
                     t[e]['onerror'] = function() {
                         //console.log("error loading image: "+ e);
