511b5644c22b1b6d0597b2c93d397712ce797860
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-09-22 06:13:24 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1293 MEGA TEST
+// v1.1294 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -10,6 +10,9 @@ var dyinglight1load = localStorage.getItem("dyinglight1load");
 function removeEmojis(string) {
     var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
     return string.replace(regex, '');
+}
+function makeUpperCaseAfterUnderline(str) {
+    return str.replace(/_\s*([a-z])/g, function(d,e) { return "_"+e.toUpperCase() });
 }
 
 Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
@@ -7501,8 +7504,8 @@ var thelegendmodproject = function(t, e, i) {
 						else if (g.includes && g.includes("_level_")){
 							var g1 = g.replace('%', '')
 							g1 = g1.replace('_level_1', '').replace('_level_2', '').replace('_level_3', '');
-							g1 = g1.charAt(0).toUpperCase() + g1.slice(1)
-							
+							g1 = g1.charAt(0).toUpperCase() + g1.slice(1);
+							g1 = makeUpperCaseAfterUnderline(g1);
 							core.registerSkin(y, null, "https://configs-web.agario.miniclippt.com/live/" + window.agarversion + g1 + ".png", null);							
 						}
 						else if (window.vanillaskins == true && window.LMAgarGameConfiguration != undefined && ogarminimapdrawer.customSkinsMap[y] == undefined) {						
