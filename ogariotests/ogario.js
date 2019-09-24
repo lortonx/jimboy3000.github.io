b76bf26262bb5afc2630ca0aacd6c53cdaac5091
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-09-24 05:06:27 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1332 MEGA TEST
+// v1.1333 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -4265,7 +4265,7 @@ var thelegendmodproject = function(t, e, i) {
                 }
             },				
             'getCachedSkin': function(t, e) {
-				//console.log(e, t[e + '_cached']);
+				/*console.log(e, t[e + '_cached']);
 				if (t[e + '_cached3']){
 					console.log('_cached3 found');
 					var today = new Date();
@@ -4275,10 +4275,10 @@ var thelegendmodproject = function(t, e, i) {
 					else if (today.getSeconds() % 2 == 1){
 						return t[e + '_cached3'] && t[e + '_cached3'].complete && t[e + '_cached3'].width ? t[e + '_cached3'] : null;
 					}
-				}
-				else{
+				}*/
+				//else{
                 return t[e + '_cached'] && t[e + '_cached'].complete && t[e + '_cached'].width ? t[e + '_cached'] : null;
-				}
+				//}
             },
             'cacheCustomSkin': function(t, e, i) {
                 if (i) {
