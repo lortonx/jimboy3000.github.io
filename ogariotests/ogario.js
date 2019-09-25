e14671ee3fae173aec737835aa06cc8764298bb9
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-09-25 23:20:26 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1352 MEGA TEST
+// v1.1353 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -7554,7 +7554,7 @@ var thelegendmodproject = function(t, e, i) {
             },
             'vanillaskins': function(y, g) {
                 if (g != null) {
-						if (LM.gameMode == ":party" && ogarminimapdrawer.customSkinsMap[y] == undefined) {){
+						if (LM.gameMode == ":party" && ogarminimapdrawer.customSkinsMap[y] == undefined){
 							y = y + "#000000";
 						}
 						//console.log(g)
