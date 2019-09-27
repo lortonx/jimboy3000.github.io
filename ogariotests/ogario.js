b43e94061f19828a9ab1fa111d2487d3e6565378
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-09-27 03:19:57 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1363 MEGA TEST
+// v1.1364 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -7573,7 +7573,7 @@ var thelegendmodproject = function(t, e, i) {
 							core.registerSkin(y, null, "https://configs-web.agario.miniclippt.com/live/" + window.agarversion + g1 + ".png", null);		
 							window.customskinanimated = true;
 						}
-						else if (window.FreskinsMap.includes(y)){
+						else if (window.Freskins && window.FreskinsMap.includes(y)){
 							for (var player = 0; player < window.FreeSkins.length; player++) {
 								if (window.FreskinsMap[player] == window.FreeSkins[player].id){									
 									core.registerSkin(y, null, "https://configs-web.agario.miniclippt.com/live/" + window.agarversion + window.FreeSkins[player].image, null);
