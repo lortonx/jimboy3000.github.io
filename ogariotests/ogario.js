53246dc0edfbd7486d8d5f0461c09f58e8a96327
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-10-08 14:50:51 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1459 MEGA TEST
+// v1.1460 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -7374,7 +7374,7 @@ var thelegendmodproject = function(t, e, i) {
                             }
                             if (16 & y) {
                                 c = true;
-								s += 1;
+								s += 5;
 								m = data.getUint32(s, true);
 								console.log('found friend id', m);
                             //s += 5;								
