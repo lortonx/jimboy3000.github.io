583686a33554e283e0792cee5053240bb9fabde1
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-10-22 20:51:27 +0300

    Update newstuff.js

--- LMexpress/newstuff.js
+++ LMexpress/newstuff.js
@@ -1,26 +1,15 @@
-<div class="input-group skin colorpicker-element">
-   <input id="skin" class="form-control" placeholder="Skin URL (imgur.com direct link)" maxlength="40">
-   <input type="hidden" id="color" value="#01d9cc" maxlength="7"><span class="input-group-addon"><i style="background-color: rgb(1, 217, 204);"></i></span>
-   <span class="input-group-btn"><button id="hide-url" class="btn active ogicon-eye"></button></span>
-</div>
-
-//$("#server-token").css("margin-top", "-10px");
-$('#server-token').parent().addClass("input-group skin colorpicker-element");
-$('#server-token').parent().css("margin-top", "-=10px");
-$('#server-token').after('<span id="Wisperer" class="input-group-btn"><button id="openskinchanger" class="btn btn-info" style="background-color: transparent; text-transform: capitalize;" onclick="BeforeReportFakesSkin();return false;" placeholder="Name" data-original-title="" title=""><i class="fa fa-wpexplorer"></i></button></span>');
-
-$('#server-token').css("width", "-=40px");
-$('#Wisperer').css("margin-top", "10px");
-$('#Wisperer').css("width", "40px");
-
-
-$('#server-join').css("width", "-=40px");
-
-
-$('#server-token').after('<span id="Wisperer" class="input-group-btn"><button id="openskinchanger" class="btn btn-info" style="background-color: transparent; text-transform: capitalize;" onclick="BeforeReportFakesSkin();return false;" placeholder="Name" data-original-title="" title=""><i class="fa fa-wpexplorer"></i></button></span>');
-$('#Wisperer').css("margin-left", "-=40px");
-$('#server-token').css("z-index", "100");
-
-$('#server-token').after('<button id="Wisperer" class="btn active ogicon-eye" ></button>');
-
-$('#server-token').after('<button id="openskinchanger" class="btn btn-info" style="background-color: transparent; text-transform: capitalize;" onclick="BeforeReportFakesSkin();return false;" placeholder="Name" data-original-title="" title=""><i class="fa fa-wpexplorer"></i></button>');
+FB.api("me/friends?limit=100","GET",{
+                        fields: "id, name, picture, canReceiveGift"
+                    }, function(response) {
+						if (response != null && response.data != null) {
+							window.facebookFriends=response.data;							
+                            var _g = 0;
+                            while (_g < response.data.length) {
+                                var friend = response.data[_g];
+                                ++_g;
+                                console.log(friend)
+                            }							
+                        } else {
+                            console.log("Error calling: FP.api");
+                        }
+});
