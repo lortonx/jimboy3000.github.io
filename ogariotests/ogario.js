4f14df7178021580f1c08bc6227a04eef7491fd4
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-10-24 00:31:54 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1502 MEGA TEST
+// v1.1503 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -4218,7 +4218,7 @@ var thelegendmodproject = function(t, e, i) {
             },
 			'findOwnedVanillaSkin': function() {
 				if (!ogarcopythelb.skinURL && window.vanillaskins && window.UserVanillaSkin && window.EquippableSkins){
-					console.log("1. skin_" + window.UserVanillaSkin);
+					//console.log("1. skin_" + window.UserVanillaSkin);
 					if (window.UserVanillaSkin.includes("skin_custom")){						
 						core.registerSkin(ogarcopythelb.nick, null, window.UserVanillaSkin, null);
 						window.UserVanillaSkin=null;
@@ -4226,7 +4226,7 @@ var thelegendmodproject = function(t, e, i) {
 					else{
                     for (var player = 0; player < window.EquippableSkins.length; player++) {
                         if (window.EquippableSkins[player].productId == "skin_" + window.UserVanillaSkin && window.EquippableSkins[player].image != "uses_spine") {	
-							console.log("2. " + window.EquippableSkins[player].image);						
+							//console.log("2. " + window.EquippableSkins[player].image);						
                             core.registerSkin(ogarcopythelb.nick, null, "https://configs-web.agario.miniclippt.com/live/" + window.agarversion + window.EquippableSkins[player].image, null);   
 							window.UserVanillaSkin=null;								
 							}
