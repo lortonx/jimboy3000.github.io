c70723fb024ce27e7b05f4b803ba294dcf6b56f1
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-11-11 20:06:31 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1570 MEGA TEST
+// v1.1571 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -8180,13 +8180,13 @@ var thelegendmodproject = function(t, e, i) {
                     }
                     //Jimboy's
                     if (16 & d) {
-                        isAgitated = true;
+                        //isAgitated = true;
                     }
                     if (32 & d) {
-                        isOwnEjected = true;
+                        //isOwnEjected = true;
                     }
                     if (64 & d) {
-                        isOtherEjected = true;
+                        //isOtherEjected = true;
                     }
                     //
                     //8 & d && (y = window.decodeURIComponent(escape(s())));
