a834fbc6e31ee3675959dcdc9d60df89566c72e3
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-11-11 02:31:06 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1552 MEGA TEST
+// v1.1553 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -7158,7 +7158,11 @@ var thelegendmodproject = function(t, e, i) {
 				window.connectionBots.send(JSON.stringify({                            
                             "message": "botscode",
                             "msg": JSON.stringify(window.botsSpawncode[window.botsSpawncodeNum])
-                 }));				
+                 }));		
+				if (window.RequestedTokens){
+					window.RequestedTokens--;
+					legendmod.sendTokenForBots();
+				}
                 //self.sendMessage(view);
             }
             if (!grecaptcha.onceLoad || grecaptcha.v2mode) {
