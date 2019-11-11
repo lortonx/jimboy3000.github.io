d12d9698aefc84c8bb7b1364f9f9d7b488e0a3eb
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2019-11-11 20:57:56 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Open Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1580 MEGA TEST
+// v1.1581 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -7166,7 +7166,7 @@ var thelegendmodproject = function(t, e, i) {
 				setTimeout(function() {	
 					legendmod.sendTimeOutTokenForBots();
 					if (!window.sendTimeOutTokenBots){
-						toastr["info"]('<b>[SERVER]:</b> Captcha solver stoped, restarting...</b>');
+						//toastr["info"]('<b>[SERVER]:</b> Captcha solver stoped, restarting...</b>');
 						window.RequestedTokens=1000;
 						legendmod.sendTokenForBots();	
 					}			
