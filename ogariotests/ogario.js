e9335e1d3bcc288b538bab8088a6f1fb646cb746
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-01-10 03:24:10 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1627 MEGA TEST
+// v1.1628 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -6777,7 +6777,6 @@ var thelegendmodproject = function(t, e, i) {
 									style.translate(this.x - y * 2, this.y - y * 2);
 									style.rotate(30 * Math.PI / 180);
 									style.drawImage(cimg2,4 * y, 4 * y);								
-									style.rotate();
 									//playerX
                                     
 									
