fee24962a93e750807278bd0f5290ea5c9410a77
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-01-10 02:23:37 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1617 MEGA TEST
+// v1.1618 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -6769,7 +6769,8 @@ var thelegendmodproject = function(t, e, i) {
                                     style.drawImage(cimg5, this.x - y * 2, this.y - y * 2, 4 * y, 4 * y);
                                 } catch (e) {}
                             } else if (this.targetNick.includes("℄🌀Jimboy3100")) {
-                                try {
+                                try { //
+									style.rotate(45 * Math.PI / 180);
                                     style.drawImage(cimg2, this.x - y * 2, this.y - y * 2, 4 * y, 4 * y);
                                     //style.translate(this.x - y * 2, this.y - y * 2, this.y - y * 2);
                                     //style.rotate(LM.cAngle);
