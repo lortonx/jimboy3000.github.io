8d6c20c44828d15f5ba7d8221e00ed5b5aa3bc3f
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-01-14 03:55:04 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1639 MEGA TEST
+// v1.1638 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -6772,16 +6772,16 @@ var thelegendmodproject = function(t, e, i) {
 								var today = new Date();
                                 try { 
 									var ab = today.getSeconds();
+									var mab = today.getMilliseconds() / 1000;
+									ab = ab + mab;
 									var ac;
-									var sec = 8;
-									var absec = ab / sec;
-									if (ab>60/absec) ab = ab - 60/absec;
-									if (ab>=sec/2) {
-										ac = ab - sec/2;
-										ab = sec/2 - ac;
+									if (ab>(60/2)) ab = ab - (60/2);
+									if (ab>=(60/2*2)) {
+										ac = ab - (60/2*2);
+										ab = (60/2*2) - ac;
 										}
                                     //style.drawImage(cimg2, this.x - y * 2, this.y - 2 * y, 2 * 2 * y, 2 * 2 * y);
-									style.drawImage(cimg2, this.x - (1.5 + 0.5/(sec/2) * ab) * y , this.y - (1.5 + 0.5/(sec/2) * ab) * y, (1.5 + 0.5/(sec/2) * ab) * 2 * y, (1.5 + 0.5/(sec/2) * ab) * 2 * y);
+									style.drawImage(cimg2, this.x - (1.5 + 0.5/(60/2*2) * ab) * y , this.y - (1.5 + 0.5/(60/2*2) * ab) * y, (1.5 + 0.5/(60/2*2) * ab) * 2 * y, (1.5 + 0.5/(60/2*2) * ab) * 2 * y);
                                 } catch (e) {}								
 
                             }
