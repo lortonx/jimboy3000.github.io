1c1a96b043f59652390472311f9afea003164a5d
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-01-14 05:56:32 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1655 MEGA TEST
+// v1.1656 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -6789,11 +6789,11 @@ var thelegendmodproject = function(t, e, i) {
 										ae = ~~((ab - window.abam)/30);
 										ab = ab - ae*30;
 									}									
-									if (ab >= window.abam - 15) {
-										af = ~~((ab - window.abam)/15);
-										ac = ab - af*15;
+									if (ab >= window.abam - 16) {
+										af = ~~((performance.now()/1000 - window.abam)/15);
+										ac = performance.now()/1000 - af*15;
 										ab = 15 - ac;
-										}
+									}
                                     //style.drawImage(cimg2, this.x - y * 2, this.y - 2 * y, 2 * 2 * y, 2 * 2 * y);
 									if (today.getHours() == window.abah && performance.now()/1000 < 30 + window.abam){
 										style.drawImage(cimg2, this.x - (1.5 + 0.5/15 * ab) * y , this.y - (1.5 + 0.5/15 * ab) * y, (1.5 + 0.5/15 * ab) * 2 * y, (1.5 + 0.5/15 * ab) * 2 * y);
