60f2522c71a3f5a6b3f5baa71fb91d330bb7c080
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-03-11 18:36:52 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1698 MEGA TEST
+// v1.1696 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -6718,7 +6718,7 @@ var thelegendmodproject = function(t, e, i) {
                             var point = this.points[i];
                             style.lineTo(point.x, point.y);
                         }
-                    } else if (defaultmapsettings.jellyPhisycs && this.isVirus && !window.testjellyPhisycsNoVirus) {
+                    } else if (defaultmapsettings.jellyPhisycs && this.isVirus) {
                         style.lineJoin = "miter"
                         var pointCount = 120;
                         var incremental = this.pi2 / pointCount;
