b8005a5e494248cf752ed7483f7f42f817673ba2
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-03-23 00:09:36 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1698 MEGA TEST
+// v1.1699 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -4350,7 +4350,8 @@ var thelegendmodproject = function(t, e, i) {
                 var a = $('#color').val();
                 ogarcopythelb.nick = t;
 					//changed
-                    ogarcopythelb.clanTag = e.trim() + "@";
+					ogarcopythelb.clanTag = e.trim();
+                    //ogarcopythelb.clanTag = e.trim() + "@";
                     ogarcopythelb.skinURL = this['checkSkinURL'](o.trim());
                     if (7 == a.length) {
 						ogarcopythelb.color = a;
