d923722eed5b403317dcd48a8cc183d205019d65
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-03-23 01:13:37 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.1702 MEGA TEST
+// v1.1703 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -5826,7 +5826,7 @@ var thelegendmodproject = function(t, e, i) {
                             if (0 == r) break;
                             msg += String.fromCharCode(r);
                         }
-						if (!msg.includes("Senpa") || !msg.includes("senpa")){
+						if (!msg.includes("Senpa") && !msg.includes("senpa")){
 							this.displayChatMessage(time, caseof, plId, msg);
 						}
 						else{
