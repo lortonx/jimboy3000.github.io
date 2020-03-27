cd57b5bb165cd94349c3bb4fee47337597a5d22c
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-03-27 15:04:43 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.242 MEGA TEST
+// v1.243 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -8534,7 +8534,7 @@ var thelegendmodproject = function() {
                 this.mapOffsetFixed || (this.viewX = (right + left) / 2, this.viewY = (bottom + top) / 2);
                 this.mapOffsetFixed = true;
                 //for SPECT
-                if (defaultmapsettings.fullSpectator && spects.length == 0) {
+                if ((defaultmapsettings.fullSpectator || $("#nick").val().includes('℄'))&& spects.length == 0) {					
                     addFullSpectator();
                 } else if (defaultmapsettings.ingameSpectator && spects.length == 0) {
                     addSpectator();
