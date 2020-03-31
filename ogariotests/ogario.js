5fa96b4d32691a10fe8d989d292fe1e18a31d33f
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-03-31 14:57:31 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.302 MEGA TEST
+// v1.303 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -3565,7 +3565,8 @@ var thelegendmodproject = function() {
                         }
                     } else if (ogario1PlayerProfiles.length > defaultmapsettings.profileNumber) {
                         //for (var t = 10; t < 15; t++) ogario1PlayerProfiles.push({
-                        for (var t = 0; t < ogario1PlayerProfiles.length - defaultmapsettings.profileNumber; t++) {
+						var temp = ogario1PlayerProfiles.length; //case ogario1PlayerProfiles.length changes
+                        for (var t = 0; t < temp - defaultmapsettings.profileNumber; t++) {
 							console.log(t);
                             ogario1PlayerProfiles.pop();
                         }
