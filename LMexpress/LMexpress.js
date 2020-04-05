dd82d8f852c79418c95f74451ed3cad25f27a86a
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-04-06 01:36:40 +0300

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,5 +1,5 @@
 /**************
- * Legend express v0.088 by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v0.089 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 var semimodVersion = "84"; // the version 1.1-> 1.11
 //fix ffa
@@ -8143,18 +8143,18 @@ function populateBanListConfig() {
 }
 
 function LMadvertisement2020(){
-$("#main-menu").hide();
-$("#skins-panel").hide();
-$("#quick-menu").hide();
-$("#exp-bar").hide();
-$("#menu-footer").before('<div id="legendhelper" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 940px; margin-top: 0px;margin-left: -240px; ">'+
+	$("#main-menu").hide();
+	$("#skins-panel").hide();
+	$("#quick-menu").hide();
+	$("#exp-bar").hide();
+	$("#menu-footer").before('<div id="legendhelper" style="background-image: url('+legbgpic+'); background-color: '+legbgcolor+'; border: 1px solid black; position:absolute;  height: 560px; width: 940px; margin-top: 0px;margin-left: -240px; ">'+
 	'<div id="Userscriptshud2" style="display:block; margin-left: 10px; margin-right: 10px;">'+ //margin-left: 10px"
 	'<br><div id="Userscriptshud3"><iframe id="customskinsIframe2" src="https://legendmod.ml/extras/banneranimatedLegendmod2020.html" width="920" height="490" >'+
     '<p>Your browser does not support iframes.</p>'+
 	'</iframe>'	+
 											'</div></div>'+
 											    '<button id= "Backtomenu" onclick="closecustomskinsIframe2020(); return false" style="margin-left: 10px;" class="btn btn-danger"  data-original-title="" title="">'+Premadeletter113+'</button>');
-('#helloContainer').css('top', 80*defaultSettings.hudScale + 'px');
+	$('#helloContainer').css('top', 80*defaultSettings.hudScale + 'px');
 }
 
 
