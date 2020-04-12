23b70fbcdbb993a3e038ba1b1caf5908fe7f4843
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-04-13 02:59:14 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.406 MEGA TEST
+// v1.407 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -5961,8 +5961,8 @@ var thelegendmodproject = function() {
                 this.sendBuffer(view);
             }
         },
-        sendPlayerPosition() {
-            if (this.isSocketOpen() && ogario.play && this.playerID) {
+        sendPlayerPosition(fake) {
+            if ((this.isSocketOpen() && ogario.play && this.playerID) || fake) {
                 const view = this.createView(17);
                 view.setUint8(0, 30);
                 view.setUint32(1, this.playerID, true);
