0205eebf4bac29547cf613ba0aa745d46254a6cb
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-04-29 00:58:47 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.457 MEGA TEST
+// v1.458 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -5548,7 +5548,8 @@ function thelegendmodproject() {
                     //console.log("recreateWS case 2:" + text);
                 }
             }
-            else if (!text && /^[a-z0-9]{5,}$/.test(token)) {
+			//else if (!text && /^[a-z0-9]{5,}$/.test(token)) {
+            else if (!text && /^[a-z0-9]{5|11,}$/.test(token)) {
                 //console.log("recreateWS case 3:" + text);
 				//text = 'wss://live-arena-' + token + '.tech.agar.io:80';
                 text = 'wss://live-arena-' + token + '.agar.io:80';
