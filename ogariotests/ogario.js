011cd5566b1ea1bbd7d57736cf848e056a8cd5af
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-04-29 00:52:14 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia
 // This is part of the Legend mod project
-// v1.456 MEGA TEST
+// v1.457 MEGA TEST
 // Game Configurations
 
 //window.testobjects = {};
@@ -5550,8 +5550,8 @@ function thelegendmodproject() {
             }
             else if (!text && /^[a-z0-9]{5,}$/.test(token)) {
                 //console.log("recreateWS case 3:" + text);
-				text = `wss://live-arena-` + token + `.tech.agar.io:80`;
-                //text = `wss://live-arena-` + token + `.agar.io:80`;
+				//text = 'wss://live-arena-' + token + '.tech.agar.io:80';
+                text = 'wss://live-arena-' + token + '.agar.io:80';
                 //text = 'wss://live-arena-' + token + '.agar.io:443'
             }
 			else if (!token.includes("s://")){
@@ -5567,7 +5567,8 @@ function thelegendmodproject() {
             var matchNew = this.ws.match(/live-arena-([\w\d]+)/);
             var text = null;
             if (matchOld) {
-                matchOld = this.ws.replace('.tech.agar.io', '').replace(/-/g, '.').match(/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,4}/);
+				matchOld = this.ws.replace('.agar.io', '').replace(/-/g, '.').match(/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,4}/);
+                //matchOld = this.ws.replace('.tech.agar.io', '').replace(/-/g, '.').match(/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,4}/);
                 if (matchOld) {
                     this.serverIP = matchOld[0];
                     text = this.serverIP;
