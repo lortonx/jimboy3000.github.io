538fca9c3a3fdd5e9a7f138aacbfcb1d8f010955
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-05-23 00:25:03 +0300

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,5 +1,5 @@
 /**************
- * Legend express v0.097 by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v0.092a by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 var semimodVersion = "92"; // the version 1.1-> 1.11
 
@@ -362,9 +362,6 @@ function PremiumUsers() {
 			}
 		}
 		localStorage.setItem("proLicenceUID", window.proLicenceUID);
-		else{
-		
-		}
 	}
 }
 
