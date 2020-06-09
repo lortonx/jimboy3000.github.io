4255dc7bf5cf8822140029984e91344cb5fecae6
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-06-09 11:15:18 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych
 // This is part of the Legend mod project
-// v1.861
+// v1.862
 
 //window.testobjects = {};
 var consoleMsgLM = "[Legend mod Express] ";
@@ -3842,7 +3842,7 @@ function thelegendmodproject() {
 					var color = ''
 					if(LM.pps<23 || LM.pps>29) color = 'color:#ff4c4c'
 					if(LM.pps<20 || LM.pps>32) color = 'color:red'
-					t += 'PPS: <span style="' + color + '">'+LM.pps+'</span>';
+					t += 'PPS: <span style=' + color + '>'+LM.pps+'</span>';
 				}				
                 this.statsHUD.textContent = t;
                 var app = this;
