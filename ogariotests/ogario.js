24888838e3e508a12cfbace0d41426baf5b65b53
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-06-21 16:36:44 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych
 // This is part of the Legend mod project
-// v1.926
+// v1.927
 
 //window.testobjects = {};
 var consoleMsgLM = "[Legend mod Express] ";
@@ -8475,11 +8475,11 @@ function thelegendmodproject() {
 						// set composite mode
 						//style.globalCompositeOperation = "source-in";
 						// draw color
-						var prevFillStyle = style.fillStyle
-						style.fillStyle = "white";
+						//var prevFillStyle = style.fillStyle
+						iconSpecialSkinEffectsByzantium.filter = 'hue-rotate(120deg) grayscale(10%) brightness(150%)';
 						style.drawImage(iconSpecialSkinEffectsByzantium, this.x + 0.6 * y, this.y + 0.7 * y, y / 3, y / 3);			
 						//style.globalCompositeOperation = "source-over";
-						style.fillStyle = prevFillStyle
+						//style.fillStyle = prevFillStyle
                     } else if (this.SpecialEffect == "Close" || this.SpecialEffect2 == "Close") {
 						style.drawImage(iconSpecialSkinEffectsClose, this.x + 0.6 * y, this.y - 0.7 * y, y / 3, y / 3);								
                     } else if (this.SpecialEffect == "Earth" || this.SpecialEffect2 == "Earth") {
