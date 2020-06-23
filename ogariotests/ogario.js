05b05fbd2c1535220281ddc65bd1c24177651a6d
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-06-23 08:56:10 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych
 // This is part of the Legend mod project
-// v1.953
+// v1.954
 
 //window.testobjects = {};
 var consoleMsgLM = "[Legend mod Express] ";
@@ -13057,7 +13057,8 @@ Game name     : ${i.displayName}<br/>
                 //await this.sleep(4); //Sonia5				
             //render() {
 			'render': async function() {
-				await drawRender.sleep(4);				
+				if (!window.fpsM) window.fpsM = 4
+				await drawRender.sleep(window.fpsM);				
                 drawRender.countFps();
                 //drawRender.renderFrame();
 				//setTimeout(function() {
