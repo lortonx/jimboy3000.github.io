17f3b4878456beb30d3065697b215fcd3df9259f
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-07-13 02:55:08 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.018
+// v2.019
 
 //window.testobjects = {};
 var consoleMsgLM = "[Legend mod Express] ";
@@ -4094,9 +4094,13 @@ function thelegendmodproject() {
                             /*t = t + ('<li><a href="#" id="pos-skin" class= "set-target" data-user-id="' + this.top5[o].id + '"style="background-color: ' + this.top5[o].color +
                                 '; width: 30px; height:30px; display: inline-block;"><span style="position: absolute; margin-left: 2px; margin-top: 2px; width: 26px; height:26px; display: inline-block;" alt="">' +
                                 teamboardskin.outerHTML + '</span>' + '</a><div style="margin-top: -30px; margin-left: 32px;">');*/
+								window.aa1=2
+								window.aa2=26
+								window.aa3=30
+								window.aa4=32
                              t = t + ('<li><a href="#" id="pos-skin" class= "set-target" data-user-id="' + this.top5[o].id + '"style="background-color: ' + this.top5[o].color +
-                                '; width: 30px; height:30px; display: inline-block;"><span style="position: absolute; margin-left: '+ 2*defaultSettings.hudScale +'px; margin-top: '+ 2*defaultSettings.hudScale +'px; width: ' + 26*defaultSettings.hudScale + 'px; height:' + 26*defaultSettings.hudScale + 'px; display: inline-block;" alt="">' +
-                                teamboardskin.outerHTML + '</span>' + '</a><div style="margin-top: ' + (-30*defaultSettings.hudScale) + 'px; margin-left: ' + 32*defaultSettings.hudScale + 'px;">');    
+                                '; width: 30px; height:30px; display: inline-block;"><span style="position: absolute; margin-left: '+ window.aa1*defaultSettings.hudScale +'px; margin-top: '+ window.aa1*defaultSettings.hudScale +'px; width: ' + window.aa2*defaultSettings.hudScale + 'px; height:' + window.aa2*defaultSettings.hudScale + 'px; display: inline-block;" alt="">' +
+                                teamboardskin.outerHTML + '</span>' + '</a><div style="margin-top: ' + (-window.aa3*defaultSettings.hudScale) + 'px; margin-left: ' + window.aa4*defaultSettings.hudScale + 'px;">');    
                             // t = t + ('<div><img src=' + teamboardskin.src + 'class="player-skin" style="border: solid 3px' + this.top5[o].color + '">' + '</a><div style="margin-top: -30px; margin-left: 32px;">');
 
                             /* if (defaultmapsettings["showTargeting"]) {
