a134783f3471526ec2215eb33e3cfc7bb9bd244b
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-07-24 03:54:01 +0300

    Delete LMexpress.sniff3.js

--- LMexpress/LMexpress.sniff3.js
+++
@@ -1,9 +0,0 @@
-
-//1. RegisterSkins
-if ( typeof core != 'undefined'){
-//core.registerSkin('fly', null, 'https://i.imgur.com/poFMdZd.png', 1, null);
-	core.registerSkin = function(a, b, c, d, e){
-	window.customskinsname=a;
-	window.customskinsurl=c;
-	}
-}
