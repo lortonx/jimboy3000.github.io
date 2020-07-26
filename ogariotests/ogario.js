01ae1edc89e2a7fd87620370cd4ecaa628fd1b93
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-07-26 06:37:18 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.068
+// v2.069
 
 
 //window.testobjects = {};
@@ -8769,9 +8769,12 @@ function thelegendmodproject() {
                                 checkVideos(node2.src, this.targetNick);
                                 try {
 									//style.beginPath();
+									style.beginPath();
+									style.globalCompositeOperation = 'destination-in';
 									style.arc(this.x - y, this.y - y, 2 * y, 0, 2 * Math.PI, true);
-									style.clip();
-									style.drawImage(window.videoSkinPlayer[node2.src], 0, 0);									
+									style.clip();									
+									style.drawImage(window.videoSkinPlayer[node2.src], 0, 0);	
+									style.closePath();									
                                     //style.drawImage(window.videoSkinPlayer[node2.src], this.x - 0.7 * y, this.y - 0.7 * y, 1.4 * y, 1.4 * y);
 									
                                 } catch (e) {}
