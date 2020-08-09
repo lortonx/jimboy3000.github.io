ade840e4fbf8cb0d55dc3f1e0a1a07aa450d92e3
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-08-09 17:24:22 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.151 bots
+// v2.152 bots
 
 
 //window.testobjects = {};
