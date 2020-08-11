3196937d70b4d8ede9f689007a4a94ef503eea86
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-08-12 02:16:02 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.235
+// v2.236
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
@@ -3435,6 +3435,7 @@ function thelegendmodproject() {
         teamPlayers: [],
         parties: [],
         chatHistory: [],
+		commandHistory: [],
         chatUsers: {},
         chatMutedUsers: {},
         chatMutedUserIDs: [],
