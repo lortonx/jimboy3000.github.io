af962a603e72b82399e09bc68f59bf69b9fb65d9
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-08-27 18:52:16 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.334
+// v2.335
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
@@ -10495,13 +10495,13 @@ function thelegendmodproject() {
 				//var wait = Math.max(3000, 1000 + message.length * 150);
 				var time = new Date().toTimeString().replace(/^(\d{2}:\d{2}).*/, '$1');
 				var caseof = 101
-				if (server || admin || mod){
+				if (name = "SERVER"){
 					caseof = 102
 				}
-				if (message.includes('}')){
-					message.split('}')[1]
+				if (name.includes('}')){
+					name.split('}')[1]
 				}
-				if (message != "SERVER WWW.IMSOLO.PRO"){
+				if (message != "WWW.IMSOLO.PRO "){
 					application.displayChatMessage(time, caseof, 1000, name + ": " + message); //this.displayChatMessage(time, caseof, plId, msg);
 				}
 				/*
