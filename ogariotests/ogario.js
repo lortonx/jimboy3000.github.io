9f7155959668570bfa04e56f157147f78ce91dd3
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-09-22 04:00:50 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.533
+// v2.534
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
@@ -12630,6 +12630,7 @@ Game name     : ${i.displayName}<br/>
         updateCells(view, offset) {
 			//window.updateCellsClock=true;
             var encode = function() {
+				console.log(view.length)
                 for (var text = '';;) {
                     var string = view.readUInt8(offset++);
                     if (0 == string) break;
