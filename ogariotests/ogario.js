5fca4bbc6bbacfe663150f0e7ff88168c3110c50
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-03 03:30:30 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.647 testing
+// v2.649 testing
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
@@ -8691,11 +8691,15 @@ function thelegendmodproject() {
 				this.size += (this.targetSize - this.size) * (time / 800);
 				if (this.size<0) this.size = 0 //fix
 			}
-            this.alpha = delay;
+            this.alpha = delay;		
             if (!this.removed) {
                 this.time = LM.time;
                 return;
             }
+			//
+			this.size = this.size * delay
+			//
+			
             if (delay == 1) {
                 var removedCells = LM.removedCells.indexOf(this);
                 if (removedCells != -1) {
