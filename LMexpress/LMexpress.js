9005b218da2a4042089924ecc30fe27c0b40d774
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-05 14:44:34 +0300

    Update LMexpress.js

--- LMexpress/LMexpress.js
+++ LMexpress/LMexpress.js
@@ -1,5 +1,5 @@
 /**************
- * Legend express v1.56 by Jimboy3100   email:jimboy3100@hotmail.com
+ * Legend express v1.57 by Jimboy3100   email:jimboy3100@hotmail.com
  *************/
 var semimodVersion = "10"; // the version 1.1-> 1.11
 
@@ -5040,8 +5040,8 @@ function initializeLM(modVersion) {
         title: "When ENABLED: Optimized mass (+/-2%) ON, Merge Timer BETA OFF. Suggested to be ENABLED for Lag reduce.",
         placement: "bottom"
     });	
-	$('#animation-value').parent().before("1. Type on browser: <b>chrome://settings/system</b> , ensure <i>Use hardware acceleration when available</i> checkbox, is <b>ENABLED</b>. 2. Prefer <b>Opera GX</b><br><br>");
-/*		
+	//$('#animation-value').parent().before("1. Type on browser: <b>chrome://settings/system</b> , ensure <i>Use hardware acceleration when available</i> checkbox, is <b>ENABLED</b>. 2. Prefer <b>Opera GX</b><br><br>");
+		
 	$('#animation-value').parent().before(
         '<label style="float:left; padding-left:0; padding-right:0">' + textLanguage.unlockedFPS + '<select id="FrameWorkOption" class="form-control" onchange="changeFrameWork();" required="" data-original-title="" title="" style="display:inline; width: 70%; float:right;" >' +        
 		'<option value="4" data-itr="">Limited Rate + 4ms delay (suggested)</option>' +		
@@ -5059,7 +5059,7 @@ function initializeLM(modVersion) {
         title: "Type on chrome: chrome://settings/system , ensure Use hardware acceleration when available checkbox, is ENABLED",
         placement: "bottom"
     });
-*/	
+	
     $("#videoSkins").parent().parent().tooltip({    
         title: "For more info on how to use video skins visit: https://legendmod.ml/developers/ videoskins.html",
         placement: "top"
