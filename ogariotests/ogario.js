a660c57107bec4b635f1c3cb079418ec6276c8dd
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-07 01:26:15 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.650 testing
+// v2.651 testing
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
@@ -10106,14 +10106,15 @@ function thelegendmodproject() {
                     let d = ~~((Date.now() / 40) % 8),
                         distance = 50000;					
 					if (window.followStraight){
-						if (this.playerX - this.cursorX > 0 && this.playerY - this.cursorY > 0 ) d = 3
-						else if (this.playerX - this.cursorX < 0 && this.playerY - this.cursorY > 0) d = 5
-						else if (this.playerX - this.cursorX < 0 && this.playerY - this.cursorY < 0) d = 7
-						else if (this.playerX - this.cursorX > 0 && this.playerY - this.cursorY < 0) d = 1
-						else if (this.playerX - this.cursorX < 0) d = 6 
-						else if (this.playerX - this.cursorX > 0) d = 2
-						else if (this.playerY - this.cursorY > 0) d = 4
-						else if (this.playerY - this.cursorY < 0) d = 0
+						
+						if (this.cursorX - this.playerX > 0 && this.cursorY - this.playerY > 0 ) d = 3
+						else if (this.cursorX - this.playerX < 0 && this.cursorY - this.playerY > 0) d = 5
+						else if (this.cursorX - this.playerX < 0 && this.cursorY - this.playerY < 0) d = 7
+						else if (this.cursorX - this.playerX > 0 && this.cursorY - this.playerY < 0) d = 1
+						else if (this.cursorX - this.playerX < 0) d = 6 
+						else if (this.cursorX - this.playerX > 0) d = 2
+						else if (this.cursorY - this.playerY > 0) d = 4
+						else if (this.cursorY - this.playerY < 0) d = 0
 					}			
                     switch (d) {
                         case 7:
