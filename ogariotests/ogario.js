f9d82d161caeafcb172006157046af9fe1c6b75b
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-09 23:10:31 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.656 testing
+// v2.657 testing
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
@@ -2836,6 +2836,8 @@ iconSpecialSkinEffectsSurvivor = new Image;
 iconSpecialSkinEffectsSurvivor.src = 'https://legendmod.ml/banners/iconSpecialSkinEffectsSurvivor.png';
 iconSpecialSkinEffectsTiger = new Image;
 iconSpecialSkinEffectsTiger.src = 'https://legendmod.ml/banners/iconSpecialSkinEffectsTiger.png';
+iconSpecialSkinEffectsPanicAtDisco = new Image;
+iconSpecialSkinEffectsPanicAtDisco.src = 'https://legendmod.ml/banners/iconSpecialSkinEffectsTiger.png';
 if (dyinglight1load == "yes") {
     cimgDyingLight = new Image;
     cimgDyingLight.src = defaultSettings.commanderImageDyingLight;
@@ -9207,6 +9209,14 @@ function thelegendmodproject() {
                     style.drawImage(iconSpecialSkinEffectsTiger, this.x - 1.1 * y, this.y - 1.3 * y, y / 1.5, y / 1.5);
                 } else if (this.targetNick.includes("The Dying Light") || this.SpecialEffect == "RedArrow" || this.SpecialEffect2 == "RedArrow") {
                     style.drawImage(cimg5, this.x - 2 * y, this.y - 2 * y, 2 * 2 * y, 2 * 2 * y);
+                } else if (this.SpecialEffect == "PanicAtDisco" || this.SpecialEffect2 == "PanicAtDisco") {
+                    style.drawImage(iconSpecialSkinEffectsPanicAtDisco, this.x - window.xx1 * y, this.y - window.xx2 * y, y/window.xx3, y/window.xx4);	
+                    if (!window.xx1) window.xx1 = 0.95
+                    if (!window.xx2) window.xx2 = 5.3/4
+                    if (!window.xx3) window.xx3 = 0.5
+                    if (!window.xx4) window.xx4 = 4					
+                    //style.drawImage(iconSpecialSkinEffectsPanicAtDisco, this.x - 2 * y, this.y - 2 * y, 2 * 2 * y, 2 * 2 * y);					
+				
                 } else if (this.SpecialEffect == "WhiteArrow" || this.SpecialEffect2 == "WhiteArrow") {
                     //style.drawImage(cimg2, this.x - y * 2, this.y - 2 * y, 2 * 2 * y, 2 * 2 * y);
 
