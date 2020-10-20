109849f729816f154b5c37c67b8df69a80ee5fa2
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-20 10:01:50 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.737 testing
+// v2.738 testing
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
