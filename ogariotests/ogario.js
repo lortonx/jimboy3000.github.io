c19eed3feba45ae3eeba34b1169c7dcb62c99095
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-20 11:58:08 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.753 testing
+// v2.754 testing
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
@@ -3284,7 +3284,7 @@ window.MouseClicks=[];
             });
         },	
 		addPresetBox3(id, name, options, value, callback) {
-            for (var option in $(id).before('<div class=\"select-wrapper\" style="display:inline-block; width: 40%; margin-Right: 10px"><select id=\"' + name + '\" class=\"form-control\"></select></div>'), options) {
+            for (var option in $(id).before('<div class=\"select-wrapper\" style="display:inline-block; width: 48%; margin-Left: 10px"><select id=\"' + name + '\" class=\"form-control\"></select></div>'), options) {
                 Object.values(options) && $('#' + name).append('<option value=\"' + option + '\">' + options[option]['label'] + '</option>');
 				//options.hasOwnProperty(option) && $('#' + name).append('<option value=\"' + option + '\">' + options[option]['name'] + '</option>');
             }
