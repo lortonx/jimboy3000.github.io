e88ccb18cee7fdfbb34e2a86969c711c8ff47816
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-20 06:18:20 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,7 +1,7 @@
 // Source script
 // Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 // This is part of the Legend mod project
-// v2.717 testing
+// v2.718 testing
 
 //window.testobjects = {};
 var consoleMsgLM = "[Client] ";
@@ -3140,7 +3140,7 @@ if (userLanguage && displayText.hasOwnProperty(userLanguage)) {
     lang = userLanguage;
 }
 var textLanguage = displayText[lang];
-
+defineChatCommand();
 function defineChatCommand() {
 	window.chatCommand = {
 		comm0: textLanguage.comm0,
