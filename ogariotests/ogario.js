ab50bfefb2fad76cd2ba580e4f2e635295f1bfb3
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-22 19:28:29 +0300

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.771
+v2.772
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -9785,10 +9785,7 @@ window.MouseClicks=[];
 							node2 = application.customSkinsMap[this.targetNick];
 						}
 						else{
-							if (this.targetNick==$("#nick").val()){
-								console.log(this.targetNick + "#" + this.color)
-							}
-							node2 = application.customSkinsMap[this.targetNick + "#" + this.color];
+							node2 = application.customSkinsMap[this.targetNick + this.color];
 						}
                         if (node2) {
                             if (node2.includes(".mp4") || node2.includes(".webm") || node2.includes(".ogv")) {
