4a620a4d317461d81efaf6223a1b181a5d6f0a88
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-29 07:49:30 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.814
+v2.815
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -13637,7 +13637,7 @@ Game name     : ${i.displayName}<br/>
 				this.ctxGrid.drawImage(this.canvas2, 0, 0)
 			}
 			else if (token == "showGrid"){
-				this.ctx.drawImage(this.savedCanvasGrid, 0, 0)
+				this.ctx.drawImage(this.CanvasGrid, 0, 0)
 			}
 			
 			//this.ctx.clearRect(0, 0, LM.mapSize, LM.mapSize);
