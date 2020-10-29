1bd1921188b8db873ba261b9c89995a527016647
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-29 04:59:25 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.792
+v2.793
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -13716,7 +13716,10 @@ Game name     : ${i.displayName}<br/>
 			//this.ctx2.save()
             this.canvas2Image = new Image();
             this.canvas2Image.src = this.canvas2.toDataURL();
-			this.ctx2.drawImage(this.canvas2Image, 0, 0);
+			
+            this.canvas2Image.onload = function() {
+				this.ctx2.drawImage(this.canvas2Image, 0, 0);
+            };			
 			//this.ctx2.drawImage(this.canvas2, 0, 0);
 			//this.ctx2.restore();
 			//this.ctx2 = this.ctx.getContext('2d');
