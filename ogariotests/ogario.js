f136bac9297c47b091f441910704d978eacfb3ae
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-10-29 08:58:08 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.821
+v2.822
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -13587,8 +13587,8 @@ Game name     : ${i.displayName}<br/>
 			this.canvas2.width = this.canvas.width;
 			this.canvas2.height = this.canvas.height;	
 
-			this.ctxGrid.width = this.canvas.width;
-			this.ctxGrid.height = this.canvas.height;
+			this.CanvasGrid.width = this.canvas.width;
+			this.CanvasGrid.height = this.canvas.height;
             //this.renderFrame();
         },
         setView() {
