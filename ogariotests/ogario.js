847650c2c9c8aa1fcf44b04c6a00897dc7ec0263
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-11-05 14:26:45 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.811
+v2.812
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -9424,7 +9424,7 @@ window.MouseClicks=[];
 		this.drawImageSpecialSkin = function(imageSpecial,b,c,d,e,style){
 			//if (typeof eval(a) === 'undefined') {
 				
-			if ('window.' + imageSpecial)) {	
+			if ('window.' + imageSpecial) {	
 				// variable is undefined
 				loadIconSpecialSkins(imageSpecial)				
 			}			
