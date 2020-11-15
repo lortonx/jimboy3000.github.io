b1505d50f81312c45c2e97380311d9135e0b3373
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-11-15 17:43:37 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.850
+v2.851
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -13760,7 +13760,7 @@ Game name     : ${i.displayName}<br/>
             return new Promise(resolve => setTimeout(resolve, ms));
         },
         renderFrame() {
-			drawRender.countFps();
+			this.countFps();
             this.renderStarted = performance.now()
             //'renderFrame': async function() { //Sonia5
             //await this.sleep(4); //Sonia5			
