4d6284ad18e64bb40df655f292a32b25a4e87d05
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-11-17 05:49:02 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.913
+v2.914
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -15089,6 +15089,7 @@ Game name     : ${i.displayName}<br/>
         //'renderFrame': async function() { //Sonia5
         //await this.sleep(4); //Sonia5				
         render() {
+			
 			if (defaultmapsettings.unlockedFPS == true) {
 				if (!window.abb) window.abb=1000
 				if (!window.abc) window.abc= 3				
@@ -15112,7 +15113,10 @@ Game name     : ${i.displayName}<br/>
 					}                    
                 }, 0);
 				
-            }			
+            }	
+			else{
+			drawRender.countFps()
+			drawRender.renderFrame();
 			else if(defaultmapsettings.unlockedFPS == "ultra2") { //old unlimited
 				//drawRender.render()
                 setTimeout(function() {
@@ -15149,7 +15153,8 @@ Game name     : ${i.displayName}<br/>
                     //window.requestAnimationFrame(drawRender.render);
                     drawRender.render();
                 }, window.renderDelay);
-            } 			
+            }
+			}			
         },
         init() {
             this.setCanvas();
