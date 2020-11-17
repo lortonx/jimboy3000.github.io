238e1a1ee80535dc93d133efa1d9733afe7315d6
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-11-17 17:06:20 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.917
+v2.917a
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -15091,18 +15091,20 @@ Game name     : ${i.displayName}<br/>
         render() {
 			
 			if (defaultmapsettings.unlockedFPS == true) {
-				if (!window.abb) window.abb=10000
-				if (!window.abc) window.abc= 3				
+				
+				if (!window.abc) window.abc= 3
+				
+				//if (!window.abb) window.abb=10000				
                 setTimeout(function() {							
 					if (drawRender.lastRenderingDelay * drawRender.fps > window.abb){
-						setTimeout(function() {window.requestAnimationFrame(drawRender.render);}, 4);
+						/*setTimeout(function() {window.requestAnimationFrame(drawRender.render);}, 4);
 						drawRender.lastRenderingDelay =0;
-						//console.log("cut2")					
+						console.log("cut2")	*/				
 					}
 					else{
 						drawRender.countFps()
 						drawRender.renderFrame();					
-						for (var i = 0; i < 3; i++) {
+						for (var i = 0; i < 4; i++) {
 							if (drawRender.lastRenderingDelay<=window.abc + window.abc*i){
 							//if (drawRender.lastRenderingDelay<=10 && drawRender.averageRenderTime && parseFloat(drawRender.averageRenderTime) < 65 - i * 10){
 								drawRender.countFps()
