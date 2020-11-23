63d0945bf0ea698f4944dd86fa33af2ed786a9c2
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-11-23 04:39:29 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.920
+v2.919
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -14542,10 +14542,9 @@ Game name     : ${i.displayName}<br/>
                 }
             } 
 			else {
-				ctx.beginPath();
                 for (var length = 0; length < food.length; length++) {
 					if (!food[length].spectator && window.fullSpectator && !defaultmapsettings.oneColoredSpectator) food[length].invisible = true
-					//here
+					ctx.beginPath();
                     if (!food[length].invisible) {				
                         var x = food[length].x;
                         var y = food[length].y;
@@ -14560,17 +14559,19 @@ Game name     : ${i.displayName}<br/>
 						else{
 							ctx.arc(x, y, food[length].size + defaultSettings.foodSize, 0, this.pi2, false);
 						}
-						if (defaultmapsettings.rainbowFood){ 							
-							ctx.fillStyle = food[length].color							
+						if (defaultmapsettings.rainbowFood){ 
+							
+							ctx.fillStyle = food[length].color
+							
 						}
                     }
 				if (!defaultmapsettings.rainbowFood){ 				
 					ctx.fillStyle = defaultSettings.foodColor;
 				}
                 //ctx.globalAlpha = 1;
-                //here				
+                ctx.fill();					
                 }
-				ctx.fill();	
+
             }
             if (reset) {
                 food = [];
