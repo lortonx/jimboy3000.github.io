4d89b633cb9c934214195913c2a1527684fb2928
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-11-27 06:34:47 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.919
+v2.920
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -13779,7 +13779,7 @@ Game name     : ${i.displayName}<br/>
 			//window.particleSprite[i].pivot.set(1000, 1000);
 			// ADD SPRITE TO STAGE
 			this.pixiCanvas.stage.addChild(this.particleSprite[i]);
-		}		
+		},
         resizeCanvas() {
             this.canvasWidth = window.innerWidth;
             this.canvasHeight = window.innerHeight;
