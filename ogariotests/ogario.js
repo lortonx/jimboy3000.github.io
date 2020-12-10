bc39dc390e5df59f3fb5a56a99540754fc890406
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-12-10 02:18:48 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.952
+v2.953
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -14600,11 +14600,11 @@ Game name     : ${i.displayName}<br/>
 							ctx.drawImage(this.pellet, x, y);
 						}
 						else{
-							if (!this.pelletColored[color]){ 
+							if (!this.pelletColored[food[length].color]){ 
 								this.preDrawPelletColors(food[length].color);
 							}
 							else{
-								ctx.drawImage(this.pelletColored[color], x, y);
+								ctx.drawImage(this.pelletColored[food[length].color], x, y);
 							}
 						}
 						
