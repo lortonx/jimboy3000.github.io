ab7a142319bdb917f54ac0a5fba48d928476e049
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-12-17 12:09:49 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.975
+v2.976
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -9848,7 +9848,7 @@ window.MouseClicks=[];
                     return;
                 }
              
-            if (defaultmapsettings.transparentCells && defaultSettings.cellsAlpha < 0.99)) {
+            if (defaultmapsettings.transparentCells && defaultSettings.cellsAlpha < 0.99) {
                 style.globalAlpha *= defaultSettings.cellsAlpha;
                 s = true;
             }
