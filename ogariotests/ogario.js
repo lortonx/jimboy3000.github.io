a678da9f05c12856d0cffd008a29f9edf90e0324
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2020-12-18 04:37:49 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v2.989
+v2.990
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -9890,7 +9890,7 @@ window.MouseClicks=[];
 					window.drawRender.preDrawCellsColors(color);
 				}
 				else{
-					style.drawImage(window.drawRender.cellsColored[color], this.x, this.y, this.size, this.size);
+					style.drawImage(window.drawRender.cellsColored[color], this.x, this.y, this.size*4, this.size*4);
 					//style.drawImage(window.drawRender.cellsColored[color], this.x, this.y);
 				}				
             } else {
