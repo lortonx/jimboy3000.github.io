0f05ba0e95bf90953d525fa40f4b8180e4de10b7
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2021-01-05 09:03:15 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v3.018
+v3.019
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -13845,13 +13845,14 @@ Game name     : ${i.displayName}<br/>
             //if (defaultmapsettings.waves ) {
 			if (LM.Waves && LM.Waves && LM.Waves.length>0) {	
 				this.drawWaves();
-            }				
+            }	
+			this.drawHelpers();			
             this.drawFood();
             if (LM.playerCellsMulti.length) {
                 this.calMinMaxMulti();
             }
             this.calMinMax();
-            this.drawHelpers();
+            
             this.drawGhostCells();
             for (var i = 0; i < LM.removedCells.length; i++) {
                 LM.removedCells[i].draw(this.ctx, true);
