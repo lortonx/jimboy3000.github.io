01a79e52bbcdf3f243f86a5d9fdb35ee8c0eab32
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2021-02-04 03:24:45 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v3.032
+v3.033
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -14673,7 +14673,7 @@ Game name     : ${i.displayName}<br/>
 			ctx.moveTo(x, y);
 			ctx.lineTo(x, y);
 			ctx.lineTo(x, y);
-			ctx.fillStyle = color
+			ctx.strokeStyle = color;
 			ctx.stroke();
 		},
         /*drawCachedFood(t, e, i, s) {
