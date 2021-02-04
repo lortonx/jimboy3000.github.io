0776350ee223263370ba88a58dbd6bb1fa780bd2
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2021-02-04 03:06:03 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v3.029
+v3.030
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -14630,7 +14630,6 @@ Game name     : ${i.displayName}<br/>
                 }
             } 
 			else /* (this.pellet)*/{
-				//var temp = ctx.fillStyle
                 for (var length = 0; length < food.length; length++) {
 					if (!food[length].spectator && window.fullSpectator && !defaultmapsettings.oneColoredSpectator) food[length].invisible = true
 					ctx.beginPath();
@@ -14638,6 +14637,12 @@ Game name     : ${i.displayName}<br/>
                         var x = food[length].x;
                         var y = food[length].y;
                         ctx.moveTo(x, y);
+				if (defaultmapsettings.rainbowFood){ 						
+					ctx.fillStyle = food[length].color
+                }
+				else if (!defaultmapsettings.rainbowFood){ 				
+					ctx.fillStyle = defaultSettings.foodColor;
+				}						
 						if (scale < 0.08) {
                         //if (scale < 0.16) {
                             const size = food[length].size + defaultSettings.foodSize;
@@ -14646,33 +14651,23 @@ Game name     : ${i.displayName}<br/>
                             //continue;
                         }
 						else{
-							this.drawCircle(ctx, x, y, food[length].size + defaultSettings.foodSize)
+							this.drawCircle(ctx, x, y, food[length].size + defaultSettings.foodSize, food[length].color)
 							//ctx.arc(x, y, food[length].size + defaultSettings.foodSize, 0, this.pi2, false);
 						}
-						if (defaultmapsettings.rainbowFood){ 
-							
-							ctx.fillStyle = food[length].color
-							
-						}
-                    }
-				if (!defaultmapsettings.rainbowFood){ 				
-					ctx.fillStyle = defaultSettings.foodColor;
-				}
-                //ctx.globalAlpha = 1;
                 ctx.fill();					
                 }
-				//ctx.fillStyle = temp
             }
 			ctx.restore()
             if (reset) {
                 food = [];
             }
         },
-		drawCircle(ctx, x, y, radius) {
+		drawCircle(ctx, x, y, radius, color) {
 			ctx.lineWidth = radius * 2;
 			ctx.lineCap = 'round';
 			ctx.beginPath();
 			ctx.moveTo(x, y);
+			ctx.lineTo(x, y);
 			ctx.lineTo(x, y);
 			ctx.stroke();
 		},
