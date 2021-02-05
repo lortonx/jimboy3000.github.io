c4bfd6b7c16d2f872a390900cf54d36c865b3534
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2021-02-05 14:08:45 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v3.057
+v3.058
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -9811,7 +9811,7 @@ window.MouseClicks=[];
 					//this.drawCircle(style, this.x, this.y, y, this.color)
 					//if (this.isVirus || defaultmapsettings.cellContours || defaultmapsettings.transparentCells || defaultmapsettings.transparentSkins || ((this.isPlayerCell || this.playerCellsMulti) && defaultmapsettings.myTransparentSkin)){ //this is the normal function
 						style.arc(this.x, this.y, y, 0, this.pi2, false);
-						style.stroke();
+						style.fill();
 					/*}	
 					else{
 						this.drawCircle(style, this.x, this.y, y, this.color)
@@ -9821,8 +9821,8 @@ window.MouseClicks=[];
 			if (!node) style.closePath();
 			//17/12/2020
 			if (!node && this.size <= 38 && this.nick == "" && !this.isVirus && !this.isPlayerCell){
-               style.fillStyle = this.color;
-               style.fill();	
+               //style.fillStyle = this.color;
+               //style.fill();	
 				
 				style.restore();
 				return
