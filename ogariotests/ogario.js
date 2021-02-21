da157cc01482cc8bf6f0d7aa4b482656b1406829
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2021-02-21 11:16:28 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v3.070
+v3.071
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -12779,8 +12779,8 @@ Game name     : ${i.displayName}<br/>
                     this.setMapOffset(this.viewMinX, this.viewMinY, this.viewMaxX, this.viewMaxY);
 
                     if (~~(this.viewMaxX - this.viewMinX) === LM.mapSize && ~~(this.viewMaxY - this.viewMinY) === LM.mapSize) {
-						window.userBots.offsetX = this.mapOffsetX;
-                        window.userBots.offsetY = this.mapOffsetY;	
+						window.userBots.offsetX = LM.mapOffsetX;
+                        window.userBots.offsetY = LM.mapOffsetY;	
                         //window.userBots.offsetX = (this.viewMinX + this.viewMaxX) / 2;
                         //window.userBots.offsetY = (this.viewMinY + this.viewMaxY) / 2;
                     }
