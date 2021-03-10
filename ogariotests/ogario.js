0d89d6b7d8f58a44d663578bdef45fbabc1fba5e
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2021-03-10 04:02:59 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
 /* Source script
-v3.093
+v3.094
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
@@ -12982,7 +12982,10 @@ Game name     : ${i.displayName}<br/>
                 this.mapOffset = 0
             } 		
 			if (!this.mapOffsetFixed){ 
+            //if (!this.integrity || (right - left) > (this.mapSize - 142) && (bottom - top) > (this.mapSize - 142)) { //2020 jimboy3100
+			console.log(right - left, bottom - top);
             if (!this.integrity || (right - left) > (this.mapSize - 142) && (bottom - top) > (this.mapSize - 142)) { //2020 jimboy3100
+
 				//if (this.integrity || this.ws.includes("replay")) {
 				if (this.integrity || temp2) {
                     this.mapOffsetX = this.mapOffset - right;
