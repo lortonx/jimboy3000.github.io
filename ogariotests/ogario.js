e9d0e4284f78321b35b2d98a3767db61a7d56b3f
jimboy3000 <26803909+jimboy3000@users.noreply.github.com>
2023-02-19 20:43:07 +0200

    Update ogario.js

--- ogariotests/ogario.js
+++ ogariotests/ogario.js
@@ -1,5 +1,5 @@
+window.OgVer=3.131;
 /* Source script
-v3.131
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
